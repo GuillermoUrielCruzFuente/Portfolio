3a29fde7145ab4b136be871d4353db315436acf6
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-04-12 18:08:32 -0500

    home design 80%, mediaqueries pending

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from 'react'
-import Lottie from 'lottie-web'
+import Lottie, { AnimationItem } from 'lottie-web'
 
 //data imports
 import logoAnimation from '../static/lottie/logo.json'
+import earthVideo from '../static/video/earth.mp4'
 
 //styles imports
 import '../scss/Home.scss'
@@ -11,6 +12,7 @@ export default function Home() {
 
 	useEffect(() => {
 		let homeShapeRef = document.getElementById('home-shape') as HTMLElement
+		let homeVideo = document.getElementById('home-video') as HTMLVideoElement
 
 		let logoAnim = Lottie.loadAnimation({
 			container: document.getElementById('lottie-animation') as HTMLElement,
@@ -30,29 +32,37 @@ export default function Home() {
 		document.getElementsByTagName('html')[0].style.overflow = 'auto'
 
 		setTimeout(() => {
-			logoAnim.play()
 			homeShapeRef.style.animation = 'home-shape-in 1s ease 300ms forwards'
-		}, 1000);
+			
+			homeShapeRef.addEventListener('animationend', (animation: AnimationEvent) => {
+				if (animation.animationName === 'home-shape-in') {
+					homeVideo.style.opacity = '1'
+					homeVideo.play()
+					logoAnim.play()
+				}
+			})
+		}, 100);
 
 		return () => {
 			logoAnim.destroy()
+			homeVideo.remove()
 		}
-	})
+	}, [])
 
 	return (
-		<div className="App">
-			<header className='App-header'>
-				<p className='big-text'>Hola!, soy</p>
-				<div id='lottie-animation'></div>
-				<p>Desarrollador web frontend, diseñador UX/UI y cuando la ocasión lo requiere editor y motion designer.</p>
-				<button>contactar</button>
+		<header id='home-container'>
+			<p className='big-text'>Hola!, soy</p>
+			<div id='lottie-animation'></div>
+			<p>Desarrollador web frontend, diseñador UX/UI y cuando la ocasión lo requiere editor y motion designer.</p>
+			<button>contactar</button>
 
-				<svg id='home-shape' viewBox="0 0 1389.987 1080" preserveAspectRatio='none' fill='#1b0221'>
-					<path d="M1990,1140H3379.987L2950,2220H1990Z" transform="translate(-1990 -1140)" />
-				</svg>
-			</header>
+			<svg id='home-shape' viewBox="0 0 1389.987 1080" preserveAspectRatio='none' fill='#1b0221'>
+				<path d="M1990,1140H3379.987L2950,2220H1990Z" transform="translate(-1990 -1140)" />
+			</svg>
 
-
-		</div>
+			<div className="full-container">
+				<video id='home-video' src={earthVideo} muted autoPlay playsInline loop></video>
+			</div>
+		</header>
 	)
 }
