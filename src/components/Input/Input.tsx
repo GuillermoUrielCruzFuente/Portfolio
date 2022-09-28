da3e20ea90d3350dc07c99f8eca0d31eb2c5ee35
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-09-28 12:05:27 -0500

    added some comments to explain some code

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,9 +2,6 @@ import { FC, useRef } from 'react'
 import './Input.scss'
 
 /**
- *
- * <Input required textarea text>placeholder</Input>
- *
  * Required type, there are three cases for this, in order
  * to avoid to force the user enter all his data
  *
@@ -31,19 +28,33 @@ type UseInput = {
 	getValue: () => string
 }
 
-export type InputType = {
+export type InputConfig = {
 	name: string
 	img: string
 	validation: 'required' | 'semi-required' | 'not-required'
 	inputType: 'textarea' | 'text' | 'email' | 'tel'
 }
 
+/**
+ * Create an animated input field with the required configuration
+ * @param InputConfig object with all the necessary information for  the input.
+ * - name: submit name and placeholder
+ * - img: input left icon
+ * - validation: this value can be
+ * 'required' for inputs that can't be empty and have a validation method
+ * 'semi-required' for inputs that can be empty but once filled have a validation method
+ * 'not-required' for inputs that can be empty and don't have a validation method
+ * - inputType: can be 'textarea', 'text', 'email' and 'text'
+ * @returns Object that contains the JSX.Element in this case an input
+ * and the getValue() method that provide to the parent an easy way to
+ * access the input value
+ */
 export const useInput = ({
 	name,
 	img,
 	validation,
 	inputType,
-}: InputType): UseInput => {
+}: InputConfig): UseInput => {
 	const inputRef = useRef<HTMLInputElement>(null)
 	const areaRef = useRef<HTMLTextAreaElement>(null)
 
