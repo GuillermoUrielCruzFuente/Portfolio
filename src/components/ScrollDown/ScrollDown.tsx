2c9fc75a43bff2c2da7a760ecc658ca6b9e467ce
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-10-14 01:38:14 -0500

    Scroll component ready

--- src/components/ScrollDown/ScrollDown.tsx
+++ src/components/ScrollDown/ScrollDown.tsx
@@ -1,12 +1,45 @@
+import { useEffect, useRef, useState } from 'react'
+import { CSSTransition } from 'react-transition-group'
 import './ScrollDown.scss'
 
 const ScrollDown = () => {
+	const [scrollState, setScrollState] = useState(false)
+	const scrollRef = useRef<HTMLDivElement>(null)
 
-    return (
-		<div className="scroll">
-			<p>scroll</p>
-			<span id="scroll-line"></span>
-		</div>
+	const hideOnScroll = () => {
+		if (window.scrollY >= 100) {
+			setScrollState(false)
+		} else {
+			setScrollState(true)
+		}
+	}
+
+	useEffect(() => {
+		setTimeout(() => setScrollState(true), 300)
+
+		window.addEventListener('scroll', hideOnScroll)
+
+		return () => {
+			window.removeEventListener('scroll', hideOnScroll)
+		}
+	}, [])
+
+	return (
+		<CSSTransition
+			mountOnEnter
+			unmountOnExit
+			in={scrollState}
+			timeout={800}
+			nodeRef={scrollRef}
+			classNames="scroll"
+		>
+			<div className="scroll" ref={scrollRef}>
+				<p>scroll</p>
+				<div className="line-container">
+					<span id="line"></span>
+				</div>
+			</div>
+		</CSSTransition>
 	)
 }
 
