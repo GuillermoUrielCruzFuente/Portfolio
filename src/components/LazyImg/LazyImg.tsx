8650e12f2ee9453f0d175f0fd2af3c7ec8b0d68f
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-10-18 00:00:11 -0500

    lazy img component updated

--- src/components/LazyImg/LazyImg.tsx
+++ src/components/LazyImg/LazyImg.tsx
@@ -1,64 +1,72 @@
-import { FC, useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export type LazyImgPath = {
-	fullImagePath: string
-	thumbnailPath: string
+	imagePath: string
 }
 
-const LazyImg: FC<LazyImgPath> = ({ fullImagePath, thumbnailPath }) => {
-	const collageRef = useRef<HTMLImageElement>(null)
+const intersectionOptions: IntersectionObserverInit = {
+	root: null,
+	rootMargin: '200px',
+	threshold: [0, 0.5],
+}
+
+const LazyImg = ({ imagePath }: LazyImgPath) => {
+	const imageRef = useRef<HTMLImageElement>(null)
 	const blur = useRef<HTMLDivElement>(null)
 
-	const intersectionOptions: IntersectionObserverInit = {
-		root: null,
-		rootMargin: '200px',
-		threshold: 0,
-	}
-
-	useEffect(() => {
-		const observer = new IntersectionObserver(
-			computeEntries,
-			intersectionOptions
-		)
-
-		collageRef.current
-			? observer.observe(collageRef.current)
-			: console.log('download image failed')
-
-		return () => {
-			collageRef.current
-				? observer.unobserve(collageRef.current!)
-				: undefined
-		}
-	}, [])
+	// const [isImageLoaded, setIsImageLoaded] = useState(false)
+	let isImageLoaded = false
 
 	const computeEntries: IntersectionObserverCallback = (
-		entries: Array<IntersectionObserverEntry>,
-		observer: IntersectionObserver
+		entries: Array<IntersectionObserverEntry>
 	) => {
 		entries.forEach((entry) => {
-			if (entry.isIntersecting) {
-				loadFullImage()
-				observer.unobserve(collageRef.current!)
+			// console.log(`image -> ${imagePath} at ${entry.intersectionRatio}`)
+			// if (entry.isIntersecting) {
+			// 	downloadImage()
+			// }
+
+			if (entry.intersectionRatio > 0 && entry.intersectionRatio < 0.5) {
+				downloadImage()
+				console.log('entry at ratio 0', entry.intersectionRatio)
+			} else if (entry.intersectionRatio >= 0.5 && isImageLoaded) {
+				runEnterAnimation()
+				imageRef.current && observer.unobserve(imageRef.current)
+				console.log('entry at ratio 0.5', entry.intersectionRatio)
 			}
+
+			// if (entry. >= 1.5) {
+			// 	console.log('1,5')
+			// }
 		})
 	}
 
-	const loadFullImage = () => {
-		collageRef.current
-			? (collageRef.current.src = fullImagePath)
-			: undefined
+	const observer = new IntersectionObserver(
+		computeEntries,
+		intersectionOptions
+	)
+
+	useEffect(() => {
+		imageRef.current
+			? observer.observe(imageRef.current)
+			: console.log('download image failed')
+
+		return () => {
+			imageRef.current && observer.unobserve(imageRef.current)
+		}
+	}, [])
+
+	const downloadImage = () => {
+		imageRef.current && (imageRef.current.src = imagePath)
 	}
 
 	const handleImageLoad = () => {
-		const currentPath = collageRef.current!.src.slice(
-			collageRef.current!.src.length - fullImagePath.length,
-			collageRef.current!.src.length
-		)
-
-		if (currentPath === fullImagePath) {
-			deleteBlur()
-		}
+		// deleteBlur()
+		// runEnterAnimation()
+		// setIsImageLoaded(true)
+		isImageLoaded = true
+		//once the image is loaded the observer is unnecessary
+		// imageRef.current && observer.unobserve(imageRef.current)
 	}
 
 	const deleteBlur = () => {
@@ -67,15 +75,19 @@ const LazyImg: FC<LazyImgPath> = ({ fullImagePath, thumbnailPath }) => {
 			: undefined
 	}
 
+	const runEnterAnimation = () => {
+		imageRef.current &&
+			imageRef.current.classList.replace('no-loaded', 'loaded')
+	}
+
 	return (
 		<div className="project-collage-main">
 			<div className="project-collage-img-container">
-				<div ref={blur} className="project-collage screen-blur"></div>
+				{/* <div ref={blur} className="project-collage screen-blur"></div> */}
 
 				<img
-					ref={collageRef}
-					className="project-collage"
-					src={thumbnailPath}
+					ref={imageRef}
+					className="project-collage no-loaded"
 					onLoad={handleImageLoad}
 					alt="project image"
 				/>
