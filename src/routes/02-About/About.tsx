97857222498cce08461681cd2959a04d94ec34c7
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-10-18 22:09:20 -0500

    code style

--- src/routes/02-About/About.tsx
+++ src/routes/02-About/About.tsx
@@ -4,7 +4,9 @@ import { useLocation } from 'react-router-dom'
 import { CSSTransition } from 'react-transition-group'
 
 //components
-import AboutCard, { AboutCardContent } from '../../components/AboutCard/AboutCard'
+import AboutCard, {
+	AboutCardContent,
+} from '../../components/AboutCard/AboutCard'
 import AnimatedTitle from '../../components/AnimatedTitle/AnimatedTitle'
 import IntersectionContainer from '../../components/IntersectionContainer/IntersectionContainer'
 import ScrollDown from '../../components/ScrollDown/ScrollDown'
@@ -77,11 +79,14 @@ const About = () => {
 					</p>
 
 					<p className="about-description">
-						Mi nombre es <span className="accent">Guillermo Cruz</span>, desarrollador
-						FRONTEND desde hace más de 4 años, he desarrollado distintas habilidades que
-						me permiten construir sitios web atractivos, siempre con la ayuda de
-						tecnologías modernas, generando un flujo de trabajo óptimo y una base de
-						código apegada a los estándares que exige la industria.
+						Mi nombre es{' '}
+						<span className="accent">Guillermo Cruz</span>,
+						desarrollador FRONTEND desde hace más de 4 años, he
+						desarrollado distintas habilidades que me permiten
+						construir sitios web atractivos, siempre con la ayuda de
+						tecnologías modernas, generando un flujo de trabajo
+						óptimo y una base de código apegada a los estándares que
+						exige la industria.
 					</p>
 
 					<div className="cards-container">
@@ -100,7 +105,7 @@ const About = () => {
 					<ScrollDown />
 				</header>
 
-				<main id='about-main'>
+				<main id="about-main">
 					<section id="experiecia" className="split-big-title">
 						<div className="big-title">
 							<AnimatedTitle
@@ -108,7 +113,9 @@ const About = () => {
 								alignment="left"
 								textClass="titles"
 							/>
-							<p className="desc-title left">📝Una breve explicación</p>
+							<p className="desc-title left">
+								📝Una breve explicación
+							</p>
 						</div>
 
 						<div className="description-container">
@@ -125,28 +132,42 @@ const About = () => {
 							>
 								<div>
 									<p className="right">
-										Aproximadamente <span className="accent">hace 4 años</span>{' '}
-										comencé con el desarrollo web, impulsado principalmente por
-										resolver las necesidades de una empresa enfocada en la venta
-										de agroinsumos, desarrollé su landing page y descubrí lo
-										interesante y entretenido que resultaba el{' '}
+										Aproximadamente{' '}
+										<span className="accent">
+											hace 4 años
+										</span>{' '}
+										comencé con el desarrollo web, impulsado
+										principalmente por resolver las
+										necesidades de una empresa enfocada en
+										la venta de agroinsumos, desarrollé su
+										landing page y descubrí lo interesante y
+										entretenido que resultaba el{' '}
 										<span className="accent">frontend</span>
 										.<br />
-										<br />A día de hoy he mejorado mi flujo de trabajo mediante
-										la adopción de distintas herramientas y lenguajes, por
+										<br />A día de hoy he mejorado mi flujo
+										de trabajo mediante la adopción de
+										distintas herramientas y lenguajes, por
 										ejemplo, css se ha covertido en{' '}
-										<span className="accent">SCSS</span>, JavaScript en{' '}
-										<span className="accent">TypeScript</span> y el insulso
-										LiveServer en <span className="accent">ViteJS</span>, además
-										he conocido el poder de bibliotecas como{' '}
+										<span className="accent">SCSS</span>,
+										JavaScript en{' '}
+										<span className="accent">
+											TypeScript
+										</span>{' '}
+										y el insulso LiveServer en{' '}
+										<span className="accent">ViteJS</span>,
+										además he conocido el poder de
+										bibliotecas como{' '}
 										<span className="accent">React</span>,{' '}
 										<span className="accent">Vue</span> y{' '}
-										<span className="accent">Svelte</span>, que no me atrevería
-										a mencionar que las domino, sin embargo las he adoptado
-										progresivamente en escenarios donde el posicionamiento no es
-										un problema y vaya que generan un cambio en el flujo de
-										trabajo. Probablemente mi siguiente meta sea la adopción de
-										frameworks ssr como <span className="accent">NextJS</span> o{' '}
+										<span className="accent">Svelte</span>,
+										que no me atrevería a mencionar que las
+										domino, sin embargo las he adoptado
+										progresivamente en escenarios donde el
+										posicionamiento no es un problema y vaya
+										que generan un cambio en el flujo de
+										trabajo. Probablemente mi siguiente meta
+										sea la adopción de frameworks ssr como{' '}
+										<span className="accent">NextJS</span> o{' '}
 										<span className="accent">NuxtJS</span>
 									</p>
 								</div>
@@ -169,7 +190,9 @@ const About = () => {
 								alignment="right"
 								textClass="titles"
 							/>
-							<p className="desc-title right">¿Aprendí algo en la universidad?📜</p>
+							<p className="desc-title right">
+								¿Aprendí algo en la universidad?📜
+							</p>
 						</div>
 
 						<div className="description-container">
@@ -186,38 +209,51 @@ const About = () => {
 							>
 								<div>
 									<p className="left">
-										Quizá este sea el tema menos alentador en este portafolio.
-										Comencé mis estudios universitarios como casi todos lo
-										hacen, con grandes aspiraciones y metas, si embargo una gran
-										cantidad de factores influyeron en mi deserción.
+										Quizá este sea el tema menos alentador
+										en este portafolio. Comencé mis estudios
+										universitarios como casi todos lo hacen,
+										con grandes aspiraciones y metas, si
+										embargo una gran cantidad de factores
+										influyeron en mi deserción.
 										<br />
 										<br />
-										Comencé mis estudios universitarios en la{' '}
+										Comencé mis estudios universitarios en
+										la{' '}
 										<span className="accent">
 											Universidad Autónoma Chapingo
 										</span>
 										, me apunté a la{' '}
 										<span className="accent">
-											primer generación de ingenieros mecatrónicos
-											especializados en la rama agronómica
+											primer generación de ingenieros
+											mecatrónicos especializados en la
+											rama agronómica
 										</span>
-										, es aqui donde reforcé mi gusto por la programación con
-										lenguajes como <span className="accent">C</span>,{' '}
+										, es aqui donde reforcé mi gusto por la
+										programación con lenguajes como{' '}
+										<span className="accent">C</span>,{' '}
 										<span className="accent">C++</span>,{' '}
 										<span className="accent">C#</span> y{' '}
-										<span className="accent">Java</span>, las clases de métodos
-										numéricos pasaban como agua entre mis manos y mis compañeros
-										y yo creábamos mancuernas de excelencia.
+										<span className="accent">Java</span>,
+										las clases de métodos numéricos pasaban
+										como agua entre mis manos y mis
+										compañeros y yo creábamos mancuernas de
+										excelencia.
 										<br />
 										<br />
-										Ante la llegada del <span className="accent">
+										Ante la llegada del{' '}
+										<span className="accent">
 											COVID-19
 										</span>{' '}
-										y las clases en línea muchas aspiraciones y deseos tuvieron
-										que verse truncados, pues la nueva realidad no contribuía ni
-										un poco a su realización, así decidí embarcarme en lo que
-										sería un nuevo yo, enfocado en el{' '}
-										<span className="accent">desarrollo web</span>.
+										y las clases en línea muchas
+										aspiraciones y deseos tuvieron que verse
+										truncados, pues la nueva realidad no
+										contribuía ni un poco a su realización,
+										así decidí embarcarme en lo que sería un
+										nuevo yo, enfocado en el{' '}
+										<span className="accent">
+											desarrollo web
+										</span>
+										.
 									</p>
 								</div>
 							</IntersectionContainer>
@@ -237,7 +273,9 @@ const About = () => {
 								alignment="left"
 								textClass="titles"
 							/>
-							<p className="desc-title left">🎧¿qué hago sino estoy programando?</p>
+							<p className="desc-title left">
+								🎧¿qué hago sino estoy programando?
+							</p>
 						</div>
 
 						<div className="description-container">
@@ -254,26 +292,37 @@ const About = () => {
 							>
 								<div>
 									<p className="right">
-										A lo largo de mi corta vida he desarrollado varios hobbies.
+										A lo largo de mi corta vida he
+										desarrollado varios hobbies.
 										<br />
 										<br />
 										Por ejemplo, me he enganchado a la{' '}
-										<span className="accent">acuariofilia</span> especialmente
-										al aquascaping (paisajismo en acuarios de agua dulce), he
-										pasado horas frente a un pantalla jugando{' '}
-										<span className="accent">videojuegos</span>, he doblado
-										papel por días enteros (
-										<span className="accent">Origami</span>) y sin embargo,
-										siempre hay un factor en común, la{' '}
+										<span className="accent">
+											acuariofilia
+										</span>{' '}
+										especialmente al aquascaping (paisajismo
+										en acuarios de agua dulce), he pasado
+										horas frente a un pantalla jugando{' '}
+										<span className="accent">
+											videojuegos
+										</span>
+										, he doblado papel por días enteros (
+										<span className="accent">Origami</span>)
+										y sin embargo, siempre hay un factor en
+										común, la{' '}
 										<span className="accent">música</span>.
 										<br />
 										<br />
-										Quizá resulte gracioso, pero me autodenominaría como{' '}
-										<span className="accent">melómano</span>, me encanta
-										investigar, leer y adentrarme en el mundo de la música, la
-										gente que la crea e interpreta. Podría pasar horas hablando
-										de aquellas canciones que me encantan, de su historia y de
-										los mensajes que se vierten en ellas.
+										Quizá resulte gracioso, pero me
+										autodenominaría como{' '}
+										<span className="accent">melómano</span>
+										, me encanta investigar, leer y
+										adentrarme en el mundo de la música, la
+										gente que la crea e interpreta. Podría
+										pasar horas hablando de aquellas
+										canciones que me encantan, de su
+										historia y de los mensajes que se
+										vierten en ellas.
 									</p>
 								</div>
 							</IntersectionContainer>
@@ -283,7 +332,12 @@ const About = () => {
 					<section className="years">
 						<div className="hobbies">
 							{hobbiesImages.map((img) => (
-								<img src={img} alt="" key={img} className="hobbies-img" />
+								<img
+									src={img}
+									alt=""
+									key={img}
+									className="hobbies-img"
+								/>
 							))}
 						</div>
 					</section>
