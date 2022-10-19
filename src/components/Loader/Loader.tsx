a2325c96c47bd2c4e515e3c4da9b1792f18cf592
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-10-18 21:14:02 -0500

    Loadded component added

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,23 @@
+type LoaderProps = {
+	className?: string
+}
+
+const Loader = ({ className }: LoaderProps) => {
+	return (
+		<div className={`hv-centered ${className}`}>
+			<div className="sk-cube-grid">
+				<div className="sk-cube sk-cube1"></div>
+				<div className="sk-cube sk-cube2"></div>
+				<div className="sk-cube sk-cube3"></div>
+				<div className="sk-cube sk-cube4"></div>
+				<div className="sk-cube sk-cube5"></div>
+				<div className="sk-cube sk-cube6"></div>
+				<div className="sk-cube sk-cube7"></div>
+				<div className="sk-cube sk-cube8"></div>
+				<div className="sk-cube sk-cube9"></div>
+			</div>
+		</div>
+	)
+}
+
+export default Loader
