74c7556f6aa416df322c930c6f03c7263f9113df
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-11-21 22:30:06 -0600

    serial entering test in no match route

--- src/routes/ZZ-NoMatch/NoMatch.tsx
+++ src/routes/ZZ-NoMatch/NoMatch.tsx
@@ -9,6 +9,7 @@ import useNavContext, { ContextType } from '../../hooks/useNavContext'
 
 import monsterAnimationData from '../../static/lottie/404-pet.json'
 import Lottie from 'lottie-web'
+import SerializedEntering from '../../components/SerializedEntering'
 
 export default function NoMatch() {
 	const { nav, setReadyToNavigate, navigateTo }: ContextType = useNavContext()
@@ -16,8 +17,13 @@ export default function NoMatch() {
 	const refContainer = useRef<HTMLDivElement>(null)
 	const [sectionState, setSectionState] = useState(false)
 
+	const [serialState, setSerialState] = useState(false)
+
 	useEffect(() => {
 		showContent()
+		setTimeout(() => {
+			setSerialState(true)
+		}, 1500)
 
 		return () => {
 			// monsterAnimationLottie.destroy()
@@ -51,27 +57,39 @@ export default function NoMatch() {
 	}
 
 	return (
-		<CSSTransition
-			in={sectionState}
-			nodeRef={refContainer}
-			timeout={350}
-			classNames="page"
-			mountOnEnter
-			unmountOnExit
-			onExited={() => setReadyToNavigate(true)}
-			onEntered={loadMonsterAnimation}
-		>
-			<header id="no-match">
-				<AnimatedTitle
-					alignment="right"
-					items={['Error', '404']}
-					textClass="error-text"
-				/>
+		<>
+			<CSSTransition
+				in={sectionState}
+				nodeRef={refContainer}
+				timeout={350}
+				classNames="page"
+				mountOnEnter
+				unmountOnExit
+				onExited={() => setReadyToNavigate(true)}
+				onEntered={loadMonsterAnimation}
+			>
+				<header id="no-match">
+					<AnimatedTitle
+						alignment="left"
+						items={['Error', '404']}
+						textClass="error-text"
+					/>
 
-				<p>La página a la que intentas acceder no existe</p>
+					<p>La página a la que intentas acceder no existe</p>
 
-				<div id="monster-animation"></div>
-			</header>
-		</CSSTransition>
+					<div id="monster-animation"></div>
+				</header>
+			</CSSTransition>
+
+			<SerializedEntering
+				classNames="serial"
+				enter={serialState}
+				timeout={1000}
+				delay={100}
+			>
+				<h1 key={'text'}>Hola</h1>
+				<p key={'desc'}>Soy una prueba</p>
+			</SerializedEntering>
+		</>
 	)
 }
