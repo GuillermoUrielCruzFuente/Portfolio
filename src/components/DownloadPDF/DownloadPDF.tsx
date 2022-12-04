95eb63660d96c9d950e2b6a50853ad7398e7ae51
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-12-03 23:46:53 -0600

    update cv download

--- src/components/DownloadPDF/DownloadPDF.tsx
+++ src/components/DownloadPDF/DownloadPDF.tsx
@@ -1,4 +1,4 @@
-import cv from '/GuillermoUrielCruzFuente-Resume.pdf'
+import cv from '/GuillermoUrielCruzFuente-FrontendDeveloper-CV.pdf'
 import './DownloadPDF.scss'
 import downloadIcon from '../../static/img/icons/contact/download.svg'
 
