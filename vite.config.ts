23f5e5d83761205f94a6cec75476deb60d0ea552
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-12-08 21:53:18 -0600

    add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
+import * as dns from 'dns'
 
-// https://vitejs.dev/config/
+dns.setDefaultResultOrder('verbatim')
+
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5500
-  }
+	plugins: [react()],
+	server: {
+		port: 5500,
+		open: true,
+		strictPort: true,
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@lottie': path.resolve(__dirname, './src/static/lottie'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@styles': path.resolve(__dirname, './src/scss'),
+		},
+	},
 })
+
