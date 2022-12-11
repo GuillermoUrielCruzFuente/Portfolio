f45d6254295477a3f3c81df3514feba162f5194f
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-12-10 23:51:38 -0600

    feat: replace navLink with FancyLink

--- src/components/NavLinksDesktop.tsx
+++ src/components/NavLinksDesktop.tsx
@@ -1,15 +1,13 @@
-import { NavLink } from 'react-router-dom'
 import { AppRoutes } from '@/routes'
 import '@styles/components/NavLinksDesktop.scss'
+import FancyLink from './FancyLink'
 
 const NavLinksDesktop = () => {
 	return (
 		<ol className="links-desk-container">
 			{Object.values(AppRoutes).map((route) => (
 				<li key={route.path}>
-					<NavLink className="nav-link-desk" to={route.path}>
-						{route.text}
-					</NavLink>
+					<FancyLink to={route.path}>{route.text}</FancyLink>
 				</li>
 			))}
 		</ol>
