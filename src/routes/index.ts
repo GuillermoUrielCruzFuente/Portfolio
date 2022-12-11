96bb88418e7ead75a3154cf9e006fc4ec84a8fb2
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-12-11 10:51:17 -0600

    feat: routes index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,3 +3,31 @@ export type Pages = 'Home' | 'About' | 'Projects' | 'Blog' | 'Contact'
 export type PagesDictionary<T> = {
 	[Page in Pages]: T
 }
+
+export type AppRoute = {
+	text: string
+	path: string
+}
+
+export const AppRoutes: PagesDictionary<AppRoute> = {
+	Home: {
+		path: '/',
+		text: 'Inicio',
+	},
+	About: {
+		path: '/sobre-mi',
+		text: 'Sobre mí',
+	},
+	Projects: {
+		path: '/proyectos',
+		text: 'Proyectos',
+	},
+	Blog: {
+		path: '/blog',
+		text: 'Blog',
+	},
+	Contact: {
+		path: '/contacto',
+		text: 'Contacto 👋',
+	},
+}
