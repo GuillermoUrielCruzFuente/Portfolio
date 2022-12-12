094b1ecba44373e796282466690cf1fbbf0b665a
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-12-12 14:37:04 -0600

    feat: update imports with aliases

--- src/helpers/imports/AboutImports.ts
+++ src/helpers/imports/AboutImports.ts
@@ -1,18 +1,16 @@
-import '../../scss/pages/About/About.scss'
+import HTMLImg from '@images/icons/about-experience/html.svg'
+import CSSImg from '@images/icons/about-experience/css.svg'
+import SCSSImg from '@images/icons/about-experience/sass.svg'
+import JSImg from '@images/icons/about-experience/js.svg'
+import TSImg from '@images/icons/about-experience/ts.svg'
+import ViteImg from '@images/icons/about-experience/vite.svg'
+import ReactImg from '@images/icons/about-experience/react.svg'
+import VueImg from '@images/icons/about-experience/vue.svg'
 
-import HTMLImg from '../../static/img/icons/about-experience/html.svg'
-import CSSImg from '../../static/img/icons/about-experience/css.svg'
-import SCSSImg from '../../static/img/icons/about-experience/sass.svg'
-import JSImg from '../../static/img/icons/about-experience/js.svg'
-import TSImg from '../../static/img/icons/about-experience/ts.svg'
-import ViteImg from '../../static/img/icons/about-experience/vite.svg'
-import ReactImg from '../../static/img/icons/about-experience/react.svg'
-import VueImg from '../../static/img/icons/about-experience/vue.svg'
-
-import Aquarium from '../../static/img/photos/hobbies/aquarium.jpg'
-import Origami from '../../static/img/photos/hobbies/origami.jpg'
-import Videogames from '../../static/img/photos/hobbies/mine.jpg'
-import Music from '../../static/img/photos/hobbies/music.jpg'
+import Aquarium from '@images/photos/hobbies/aquarium.jpg'
+import Origami from '@images/photos/hobbies/origami.jpg'
+import Videogames from '@images/photos/hobbies/mine.jpg'
+import Music from '@images/photos/hobbies/music.jpg'
 
 export const techImages = [
 	HTMLImg,
