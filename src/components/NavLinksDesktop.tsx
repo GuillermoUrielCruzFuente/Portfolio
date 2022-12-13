e78c72ac0f65c67d96434fc2e643fd9206d3d167
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-12-12 19:25:01 -0600

    feat: enter animation ready

--- src/components/NavLinksDesktop.tsx
+++ src/components/NavLinksDesktop.tsx
@@ -1,16 +1,46 @@
 import { AppRoutes } from '@/routes'
 import '@styles/components/NavLinksDesktop.scss'
-import FancyLink from './FancyLink'
+import FancyLink from '@components/FancyLink'
+import { motion, Variants } from 'framer-motion'
+import { useLocation } from 'react-router-dom'
+
+const mountingVariants: Variants = {
+	unmount: {
+		opacity: 0,
+		y: '-100%',
+	},
+	mount: {
+		opacity: 1,
+		y: 0,
+		transition: {
+			type: 'spring',
+			bounce: 1,
+			stiffness: 120,
+			damping: 8,
+			duration: 1,
+		},
+	},
+}
 
 const NavLinksDesktop = () => {
+	const { pathname } = useLocation()
+
 	return (
-		<ol className="links-desk-container">
+		<motion.ol
+			className="links-desk-container"
+			initial="unmount"
+			animate="mount"
+			transition={{
+				delayChildren: pathname === '/' ? 2.8 : 1.5,
+				staggerChildren: 0.06,
+			}}
+		>
 			{Object.values(AppRoutes).map((route) => (
-				<li key={route.path}>
+				<motion.li key={route.path} variants={mountingVariants}>
 					<FancyLink to={route.path}>{route.text}</FancyLink>
-				</li>
+				</motion.li>
 			))}
-		</ol>
+		</motion.ol>
 	)
 }
 
