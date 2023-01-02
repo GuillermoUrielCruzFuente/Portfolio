301b24955b0336404312dbc07506229c68e043b3
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2023-01-01 20:03:45 -0600

    feat: update lottie animation using the new custom hook and implement the logo animation state dispatcher

--- src/components/NavLogo.tsx
+++ src/components/NavLogo.tsx
@@ -1,93 +1,85 @@
-import createLogoAnimationConfig from '@/helpers/CreateLogoAnimationConfig'
-import Lottie, { AnimationItem } from 'lottie-web'
 import { useLayoutEffect, useRef } from 'react'
 import { Link, useLocation } from 'react-router-dom'
+import { useLogoAnimationState } from '@/context/LogoAnimationState'
+import useLottie from '@/hooks/useLottie'
+import logoAnimationData from '@lottie/logo.json'
 import '@styles/components/NavLogo.scss'
 
 const NavLogo = () => {
+	const [LogoAnimation, lottieLogoAnimation] = useLottie({
+		data: logoAnimationData,
+	})
+	const [_, setLogoAnimationState] = useLogoAnimationState()
 	const location = useLocation()
-	const logoContainerRef = useRef<HTMLDivElement>(null)
-	const logoAnimationRef = useRef<AnimationItem | undefined>()
+	const isHomePage = location.pathname === '/'
 	const linkRef = useRef<HTMLAnchorElement>(null)
 
 	useLayoutEffect(() => {
-		initLogoAnimation()
-		addLinkListeners()
+		setAnimationInitialFrame()
+		addLogoLinkListeners()
 
-		return cleanUp
+		return () => {
+			removeLogoLinkListeners()
+		}
 	}, [])
 
 	useLayoutEffect(() => {
-		isHomePage() ? hideLogo() : showLogo()
+		isHomePage ? hideLogo() : showLogo()
 	}, [location.pathname])
 
-	const cleanUp = () => {
-		logoAnimationRef.current?.destroy()
-		removeLinkListeners()
-	}
-
-	const isHomePage = () => location.pathname === '/'
-
-	const addLinkListeners = () => {
+	const addLogoLinkListeners = () => {
 		linkRef.current?.addEventListener('transitionend', handleLinkTransition)
+
+		lottieLogoAnimation.current.addEventListener('complete', () =>
+			setLogoAnimationState({ isComplete: true })
+		)
 	}
 
-	const removeLinkListeners = () => {
+	const removeLogoLinkListeners = () => {
 		linkRef.current?.removeEventListener(
 			'transitionend',
 			handleLinkTransition
 		)
 	}
 
-	const initLogoAnimation = () => {
-		logoAnimationRef.current = Lottie.loadAnimation(
-			createLogoAnimationConfig(logoContainerRef.current!)
-		)
-
-		setAnimationInitialFrame()
-	}
-
 	const setAnimationInitialFrame = () =>
-		logoAnimationRef.current?.goToAndStop(74, true)
+		lottieLogoAnimation.current.goToAndStop(74, true)
 
-	const isLinkInvisible = () => linkRef.current?.style.opacity === '0'
+	const isLinkInvisible = () => linkRef.current!.style.opacity === '0'
 
-	const changeLogoVisibility = ({ isVisible }: { isVisible: boolean }) => {
-		linkRef.current!.style.opacity = isVisible ? '1' : '0'
-	}
+	const changeLogoLinkVisibility = ({ isVisible }: { isVisible: boolean }) =>
+		(linkRef.current!.style.opacity = isVisible ? '1' : '0')
 
-	const wasLinkTurnedToInvisible = (event: TransitionEvent) => {
-		return event.propertyName === 'opacity' && isLinkInvisible()
-	}
+	const wasLinkTurnedToInvisible = (event: TransitionEvent) =>
+		event.propertyName === 'opacity' && isLinkInvisible()
 
-	const handleLinkTransition = (event: globalThis.TransitionEvent) => {
+	const handleLinkTransition = (event: globalThis.TransitionEvent) =>
 		wasLinkTurnedToInvisible(event) && setAnimationInitialFrame()
-	}
 
-	const changeLinkInteractivity = ({
+	const changeLogoLinkInteractivity = ({
 		isInteractive,
 	}: {
 		isInteractive: boolean
 	}) => {
-		linkRef.current?.blur()
+		linkRef.current!.blur()
 		linkRef.current!.tabIndex = isInteractive ? 1 : -1
 		linkRef.current!.style.pointerEvents = isInteractive ? 'all' : 'none'
 	}
 
 	const hideLogo = () => {
-		changeLogoVisibility({ isVisible: false })
-		changeLinkInteractivity({ isInteractive: false })
+		changeLogoLinkVisibility({ isVisible: false })
+		changeLogoLinkInteractivity({ isInteractive: false })
 	}
 
 	const showLogo = () => {
-		changeLogoVisibility({ isVisible: true })
-		changeLinkInteractivity({ isInteractive: true })
-		logoAnimationRef.current?.play()
+		changeLogoLinkVisibility({ isVisible: true })
+		changeLogoLinkInteractivity({ isInteractive: true })
+		lottieLogoAnimation.current.play()
 	}
 
 	return (
-		<Link className="nav-logo" to={'/'} ref={linkRef}>
-			<div className="logo" ref={logoContainerRef} />
+		<Link className="nav-logo" to="/" ref={linkRef}>
+			<LogoAnimation className="logo" />
 		</Link>
 	)
 }
