d33991d10c75d41327d6d3955f1704b174d8d3a0
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2023-01-01 18:52:56 -0600

    feat: add LogoAnimationState provider to the App

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import LogoAnimationStateProvider from '@/context/LogoAnimationState/provider'
 import '@styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<LogoAnimationStateProvider>
+				<App />
+			</LogoAnimationStateProvider>
 		</BrowserRouter>
 	</React.StrictMode>
 )
