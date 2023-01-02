8905a2b05a3468bca110ce86d0a11dcbda42a068
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2023-01-01 20:40:50 -0600

    feat: add a NavLinks component to contain all links for desktop navbar

--- src/components/NavLinksDesktop.tsx
+++ src/components/NavLinksDesktop.tsx
@@ -1,8 +1,9 @@
-import { AppRoutes } from '@/routes'
+import { routes } from '@/routes'
 import '@styles/components/NavLinksDesktop.scss'
 import FancyLink from '@components/FancyLink'
 import { motion, Variants } from 'framer-motion'
 import { useLocation } from 'react-router-dom'
+import { useLogoAnimationState } from '@/context/LogoAnimationState/useLogoAnimationState'
 
 const mountingVariants: Variants = {
 	unmount: {
@@ -24,6 +25,17 @@ const mountingVariants: Variants = {
 
 const NavLinksDesktop = () => {
 	const { pathname } = useLocation()
+	const isHome = pathname === '/'
+	const [logoAnimationState] = useLogoAnimationState()
+	const hasLogoAnimationPlayed = logoAnimationState.isComplete
+
+	//manage the delay value based on 2 conditions
+	//if it's home page or not
+	//and if the logo animation has been completed (home or nav)
+	const calculateDelay = (): number => {
+		const delay = isHome ? 2.8 : 1.5
+		return hasLogoAnimationPlayed ? 0 : delay
+	}
 
 	return (
 		<motion.ol
@@ -31,11 +43,11 @@ const NavLinksDesktop = () => {
 			initial="unmount"
 			animate="mount"
 			transition={{
-				delayChildren: pathname === '/' ? 2.8 : 1.5,
+				delayChildren: calculateDelay(),
 				staggerChildren: 0.06,
 			}}
 		>
-			{Object.values(AppRoutes).map((route) => (
+			{routes.map((route) => (
 				<motion.li key={route.path} variants={mountingVariants}>
 					<FancyLink to={route.path}>{route.text}</FancyLink>
 				</motion.li>
