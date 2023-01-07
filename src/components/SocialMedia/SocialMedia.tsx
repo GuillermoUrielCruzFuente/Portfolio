7e3001ce2bd36596ab6edae186beb794a090695c
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2023-01-07 01:17:12 -0600

    feat: split component code into other files. This is now a container

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -1,84 +1,18 @@
-import { MutableRefObject, useLayoutEffect, useRef } from 'react'
-import { CSSTransition } from 'react-transition-group'
-
-//styles
+import socialMediaSites from '@/data/SocialMediaData'
+import SocialMediaLink from '@components/SocialMediaLink'
 import './SocialMedia.scss'
 
-//images
-import githubIcon from '../../static/img/icons/social-media/github.svg'
-import linkedinIcon from '../../static/img/icons/social-media/linkedin.svg'
-import emailIcon from '../../static/img/icons/social-media/email.svg'
-import whatsappIcon from '../../static/img/icons/social-media/whatsapp.svg'
-
-type imgLink = {
-	url: string
-	src: string
-	ref: MutableRefObject<null>
-}
-
-type SocialMediaProp = {
+type SocialMediaProps = {
 	containerClass: string
-	state: boolean
+	state?: boolean
 }
 
-const SocialMedia = ({ containerClass, state }: SocialMediaProp) => {
-	const SocialMediaIcons: Array<imgLink> = [
-		{
-			url: 'https://github.com/GuillermoCruzFuente',
-			src: githubIcon,
-			ref: useRef(null),
-		},
-		{
-			url: 'https://www.linkedin.com/in/guillermo-uriel-cruz-fuente-b01378236/',
-			src: linkedinIcon,
-			ref: useRef(null),
-		},
-		{
-			url: 'mailto:guillermo.uriel.cruz.fuente@gmail.com',
-			src: emailIcon,
-			ref: useRef(null),
-		},
-		{
-			url: 'https://api.whatsapp.com/send?phone=525951224620&text=Hola%2C%20vengo%20de%20tu%20p%C3%A1gina%20web.',
-			src: whatsappIcon,
-			ref: useRef(null),
-		},
-	]
-
-	useLayoutEffect(() => {
-		SocialMediaIcons.forEach((icon) => {
-			const img = new Image()
-			img.src = icon.src
-		})
-	}, [])
-
-	return (
-		<div className={`social-media-main-container ${containerClass}`}>
-			{SocialMediaIcons.map((link: imgLink, i: number) => {
-				return (
-					<CSSTransition
-						in={state}
-						classNames="container"
-						timeout={{ enter: 500 + i * 50, exit: 500 }}
-						mountOnEnter
-						unmountOnExit
-						nodeRef={link.ref}
-						key={link.url}
-					>
-						<a
-							className="social-media-anchor-tag"
-							href={link.url}
-							target={'_blank'}
-							ref={link.ref}
-							style={{ transitionDelay: `${i * 50}ms` }}
-						>
-							<img className="social-media-icon" src={link.src} alt="" />
-						</a>
-					</CSSTransition>
-				)
-			})}
-		</div>
-	)
-}
+const SocialMedia = ({ containerClass }: SocialMediaProps) => (
+	<div className={`social-media-main-container ${containerClass}`}>
+		{socialMediaSites.map((site) => (
+			<SocialMediaLink key={site.name} {...site} />
+		))}
+	</div>
+)
 
 export default SocialMedia
