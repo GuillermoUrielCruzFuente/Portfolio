d8dc362d168a90138f590639e7f4c5ed26c95d4c
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-03-24 21:21:53 -0600

    style: new prettier formatting style

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import * as path from 'path'
-import * as dns from 'dns'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+import * as dns from "dns";
 
-dns.setDefaultResultOrder('verbatim')
+dns.setDefaultResultOrder("verbatim");
 
 export default defineConfig({
 	plugins: [react()],
@@ -14,12 +14,12 @@ export default defineConfig({
 	},
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, './src'),
-			'@lottie': path.resolve(__dirname, './src/static/lottie'),
-			'@components': path.resolve(__dirname, './src/components'),
-			'@styles': path.resolve(__dirname, './src/scss'),
-			'@images': path.resolve(__dirname, './src/static/img'),
-			'@routes': path.resolve(__dirname, './src/routes'),
+			"@": path.resolve(__dirname, "./src"),
+			"@lottie": path.resolve(__dirname, "./src/static/lottie"),
+			"@components": path.resolve(__dirname, "./src/components"),
+			"@styles": path.resolve(__dirname, "./src/scss"),
+			"@images": path.resolve(__dirname, "./src/static/img"),
+			"@routes": path.resolve(__dirname, "./src/routes"),
 		},
 	},
-})
+});
