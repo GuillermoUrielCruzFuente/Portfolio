0020602b25b5b800a39495eadcbc3da52d9a95dd
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-03-27 12:59:04 -0600

    refactor: replace transitionGroup with framer motion

--- src/components/ScrollDown/ScrollDown.tsx
+++ src/components/ScrollDown/ScrollDown.tsx
@@ -1,46 +1,48 @@
-import { useEffect, useRef, useState } from 'react'
-import { CSSTransition } from 'react-transition-group'
-import './ScrollDown.scss'
+import { AnimatePresence, motion, Transition } from "framer-motion";
+import { useEffect, useState } from "react";
+import "./ScrollDown.scss";
 
 const ScrollDown = () => {
-	const [scrollState, setScrollState] = useState(false)
-	const scrollRef = useRef<HTMLDivElement>(null)
+	const [scrollState, setScrollState] = useState(false);
+
+	const needToBeVisible = () => !(window.scrollY >= 100);
 
 	const hideOnScroll = () => {
-		if (window.scrollY >= 100) {
-			setScrollState(false)
-		} else {
-			setScrollState(true)
-		}
-	}
+		setScrollState(needToBeVisible());
+	};
 
 	useEffect(() => {
-		hideOnScroll()
+		// invoke the funtion at the first render in order
+		// to avoid render the component on a previous saved scroll state
+		hideOnScroll();
 
-		window.addEventListener('scroll', hideOnScroll)
+		window.addEventListener("scroll", hideOnScroll);
 
 		return () => {
-			window.removeEventListener('scroll', hideOnScroll)
-		}
-	}, [])
+			window.removeEventListener("scroll", hideOnScroll);
+		};
+	}, []);
+
+	const transitionConfig: Transition = { type: "tween", duration: 1.75, ease: "anticipate" };
 
 	return (
-		<CSSTransition
-			mountOnEnter
-			unmountOnExit
-			in={scrollState}
-			timeout={800}
-			nodeRef={scrollRef}
-			classNames="scroll"
-		>
-			<div className="scroll" ref={scrollRef}>
-				<p>scroll</p>
-				<div className="line-container">
-					<span id="line"></span>
-				</div>
-			</div>
-		</CSSTransition>
-	)
-}
+		<AnimatePresence>
+			{scrollState && (
+				<motion.div
+					initial={{ y: -40, opacity: 0 }}
+					animate={{ y: 0, opacity: 1 }}
+					exit={{ y: "100%", opacity: 0 }}
+					transition={transitionConfig}
+					className="scroll"
+				>
+					<p>scroll</p>
+					<div className="line-container">
+						<span id="line"></span>
+					</div>
+				</motion.div>
+			)}
+		</AnimatePresence>
+	);
+};
 
-export default ScrollDown
+export default ScrollDown;
