7bd6a2e1081b6a0c9c072dd40a3665b948fd453e
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-03-27 22:07:23 -0600

    refactor: replace transition-group with framer-motion

--- src/components/LazyImg/LazyImg.tsx
+++ src/components/LazyImg/LazyImg.tsx
@@ -1,119 +1,114 @@
-import { useEffect, useRef, useState } from 'react'
-import Loader from '../Loader/Loader'
-import { CSSTransition } from 'react-transition-group'
+import { useEffect, useRef, useState } from "react";
+import Loader from "../Loader/Loader";
+import { AnimatePresence, motion } from "framer-motion";
 
 export type LazyImgPath = {
-	imagePath: string
-}
+	imagePath: string;
+};
 
 // this observer config has a big margin ir order to
 // fire the image downloading before it enters the viewport
 const observerDownloaderOptions: IntersectionObserverInit = {
 	root: null,
-	rootMargin: '400px',
+	rootMargin: "400px",
 	threshold: 0,
-}
+};
 
 // this observer config has no margin and a threshold value > 0 in order to
 // fire the enter animation when the element is within the viewport
 const observerEnterAnimationOptions: IntersectionObserverInit = {
 	root: null,
-	rootMargin: '0px',
+	rootMargin: "0px",
 	threshold: 0.25,
-}
+};
 
 const LazyImg = ({ imagePath }: LazyImgPath) => {
-	const imageRef = useRef<HTMLImageElement>(null)
-	const loaderRef = useRef<HTMLDivElement>(null)
-	const [isLoaded, setIsLoaded] = useState(false)
+	const imageRef = useRef<HTMLImageElement>(null);
+	const [isLoaded, setIsLoaded] = useState(false);
 
-	const downloaderObserverCallback: IntersectionObserverCallback = (
-		entries: Array<IntersectionObserverEntry>
-	) => {
+	const downloaderObserverCallback: IntersectionObserverCallback = (entries) => {
 		entries.forEach((entry) => {
 			if (entry.isIntersecting) {
-				downloadImage()
+				downloadImage();
 
 				//once the download action is fired it is no longer necessary to observe it
-				imageRef.current && downloadObserver.unobserve(imageRef.current)
-				downloadObserver.disconnect()
+				imageRef.current && downloadObserver.unobserve(imageRef.current);
+				downloadObserver.disconnect();
 			}
-		})
-	}
+		});
+	};
 
 	const downloadObserver = new IntersectionObserver(
 		downloaderObserverCallback,
 		observerDownloaderOptions
-	)
+	);
 
 	/**
 	 * Switch between classes to create an enter animation effect
 	 * css classes defined in Projects.scss file
 	 */
 	const runEnterAnimation = () => {
-		imageRef.current &&
-			imageRef.current.classList.replace('no-loaded', 'loaded')
-	}
+		imageRef.current && imageRef.current.classList.replace("no-loaded", "loaded");
+	};
 
 	const enterAnimationObserverCallback: IntersectionObserverCallback = (
 		entries: Array<IntersectionObserverEntry>
 	) => {
 		entries.forEach((entry) => {
 			if (entry.isIntersecting) {
-				runEnterAnimation()
+				runEnterAnimation();
 
-				imageRef.current &&
-					enterAnimationObserver.unobserve(imageRef.current)
+				imageRef.current && enterAnimationObserver.unobserve(imageRef.current);
 			}
-		})
-	}
+		});
+	};
 
 	const enterAnimationObserver = new IntersectionObserver(
 		enterAnimationObserverCallback,
 		observerEnterAnimationOptions
-	)
+	);
 
 	/**
 	 * Once the component is mounted register it to the downloadObserver
 	 * and if it will be unmounted disconnect all existing observers
 	 */
 	useEffect(() => {
-		imageRef.current && downloadObserver.observe(imageRef.current)
+		imageRef.current && downloadObserver.observe(imageRef.current);
 
 		return () => {
-			downloadObserver.disconnect()
-			enterAnimationObserver.disconnect()
+			downloadObserver.disconnect();
+			enterAnimationObserver.disconnect();
 
 			// imageRef.current && downloadObserver.unobserve(imageRef.current)
-		}
-	}, [])
+		};
+	}, []);
 
 	/**
 	 * Download is triggered by setting the src property in the img element
 	 */
 	const downloadImage = () => {
-		imageRef.current && (imageRef.current.src = imagePath)
-	}
+		imageRef.current && (imageRef.current.src = imagePath);
+	};
 
 	/**
 	 * since the loader visibility depends on isLoaded state
 	 * hide it by setting that value to false
 	 */
 	const hideLoader = () => {
-		setIsLoaded(true)
-	}
+		setIsLoaded(true);
+	};
 
 	/**
 	 * Function triggered when the image is completly loaded
 	 */
 	const handleImageLoad = () => {
-		hideLoader()
+		hideLoader();
 
 		//once the image is completely loaded
 		//active the enterAnimationObserver to
 		//run the animation at the right moment
-		imageRef.current && enterAnimationObserver.observe(imageRef.current)
-	}
+		imageRef.current && enterAnimationObserver.observe(imageRef.current);
+	};
 
 	return (
 		<div className="project-collage-main">
@@ -125,22 +120,23 @@ const LazyImg = ({ imagePath }: LazyImgPath) => {
 					alt="project image"
 				/>
 
-				<CSSTransition
-					in={!isLoaded}
-					mountOnEnter
-					unmountOnExit
-					classNames="loader"
-					timeout={300}
-					nodeRef={loaderRef}
-				>
-					<div ref={loaderRef} className="img-loader">
-						<Loader />
-						<p className="loader-text">loading image...</p>
-					</div>
-				</CSSTransition>
+				<AnimatePresence>
+					{!isLoaded && (
+						<motion.div
+							className="img-loader"
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0 }}
+							transition={{ duration: 1 }}
+						>
+							<Loader />
+							<p className="loader-text">loading image...</p>
+						</motion.div>
+					)}
+				</AnimatePresence>
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default LazyImg
+export default LazyImg;
