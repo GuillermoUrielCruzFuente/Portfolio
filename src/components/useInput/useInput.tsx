717f63cb90fa4127894a99ec5d3ffaa6347b6715
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-04-01 23:56:38 -0600

    style: update to the new formatting standard

--- src/components/useInput/useInput.tsx
+++ src/components/useInput/useInput.tsx
@@ -1,20 +1,20 @@
-import { CSSProperties, useRef, useState } from 'react'
+import { CSSProperties, useRef, useState } from "react";
 
 export type UseInput = {
-	render: JSX.Element
-	getValue: () => string
-	isValid: boolean
-	shakeLabel: () => void
-}
+	render: JSX.Element;
+	getValue: () => string;
+	isValid: boolean;
+	shakeLabel: () => void;
+};
 
-export type InputValidation = Pick<UseInput, "getValue" | "isValid">
+export type InputValidation = Pick<UseInput, "getValue" | "isValid">;
 
 export type InputConfig = {
-	name: string
-	img: string
-	required: boolean
-	inputType: 'text' | 'email' | 'tel'
-}
+	name: string;
+	img: string;
+	required: boolean;
+	inputType: "text" | "email" | "tel";
+};
 
 /**
  * Create an animated input field with the required configuration
@@ -29,76 +29,68 @@ export type InputConfig = {
  * - isValid property
  * - shakeLabel() method that provide to the parent an easy way to give visual feedback to user on invalid inputs
  */
-export const useInput = ({
-	name,
-	img,
-	required,
-	inputType,
-}: InputConfig): UseInput => {
-	const inputRef = useRef<HTMLInputElement>(null)
-	const [inputState, setInputState] = useState(!required)
-	const labelRef = useRef<HTMLSpanElement>(null)
+export const useInput = ({ name, img, required, inputType }: InputConfig): UseInput => {
+	const inputRef = useRef<HTMLInputElement>(null);
+	const [inputState, setInputState] = useState(!required);
+	const labelRef = useRef<HTMLSpanElement>(null);
 
-	const getValue = () => (inputRef.current ? inputRef.current.value : '')
-	const isInputFilled = () => getValue() != ''
-	const isInputValid = () => inputRef.current?.validity.valid
+	const getValue = () => (inputRef.current ? inputRef.current.value : "");
+	const isInputFilled = () => getValue() != "";
+	const isInputValid = () => inputRef.current?.validity.valid;
 
 	const shakeLabel = () => {
-		labelRef.current!.style.animation = 'shake 400ms ease'
+		labelRef.current!.style.animation = "shake 400ms ease";
 
-		labelRef.current!.addEventListener(
-			'animationend',
-			(event: AnimationEvent) => {
-				if (event.animationName === 'shake') {
-					labelRef.current!.style.animation = 'unset'
-				}
+		labelRef.current!.addEventListener("animationend", (event: AnimationEvent) => {
+			if (event.animationName === "shake") {
+				labelRef.current!.style.animation = "unset";
 			}
-		)
-	}
+		});
+	};
 
 	const handleInputContentChange = () => {
 		if (isInputValid()) {
 			if (isInputFilled()) {
-				setInputState(true)
+				setInputState(true);
 			} else {
 				//a weird way to force the re render
-				setInputState(false)
-				setTimeout(() => setInputState(true), 10)
+				setInputState(false);
+				setTimeout(() => setInputState(true), 10);
 			}
 		} else {
-			setInputState(false)
+			setInputState(false);
 		}
-	}
+	};
 
 	const VALIDATION_DESCRIPTIONS = {
-		text: 'Ingresa tu nombre.',
-		tel: 'Ingresa tu número telefónico a 10 dígitos. Ej. 5551588911',
-		email: 'Ingresa una dirección de correo válida. Ej. correo@gmail.com',
-	}
+		text: "Ingresa tu nombre.",
+		tel: "Ingresa tu número telefónico a 10 dígitos. Ej. 5551588911",
+		email: "Ingresa una dirección de correo válida. Ej. correo@gmail.com",
+	};
 
 	const LABEL_STATE_COLORS = {
-		OK: '#99fb9c',
-		ERROR: '#fb9999',
-		NEUTRAL: '#f5c8ff',
-	}
+		OK: "#99fb9c",
+		ERROR: "#fb9999",
+		NEUTRAL: "#f5c8ff",
+	};
 
 	const getInlineValidationDescription = (): string => {
-		return VALIDATION_DESCRIPTIONS[inputType]
-	}
+		return VALIDATION_DESCRIPTIONS[inputType];
+	};
 
 	const computeLabelColor = (): CSSProperties => {
 		if (inputState) {
 			if (isInputFilled()) {
-				return { color: LABEL_STATE_COLORS.OK }
+				return { color: LABEL_STATE_COLORS.OK };
 			} else {
-				return { color: LABEL_STATE_COLORS.NEUTRAL }
+				return { color: LABEL_STATE_COLORS.NEUTRAL };
 			}
 		} else if (required) {
-			return { color: LABEL_STATE_COLORS.ERROR }
+			return { color: LABEL_STATE_COLORS.ERROR };
 		} else {
-			return { color: LABEL_STATE_COLORS.NEUTRAL }
+			return { color: LABEL_STATE_COLORS.NEUTRAL };
 		}
-	}
+	};
 
 	const input = (
 		<>
@@ -111,13 +103,17 @@ export const useInput = ({
 					placeholder={name}
 					type={inputType}
 					onChange={handleInputContentChange}
-					pattern={inputType === 'tel' ? '[0-9]{10}' : undefined}
+					pattern={inputType === "tel" ? "[0-9]{10}" : undefined}
 					required={required}
 				/>
 
 				<label>{name}</label>
 
-				<img className="label-icon" src={img} alt={`${name} icon`} />
+				<img
+					className="label-icon"
+					src={img}
+					alt={`${name} icon`}
+				/>
 			</div>
 
 			<span
@@ -128,14 +124,14 @@ export const useInput = ({
 				{getInlineValidationDescription()}
 			</span>
 		</>
-	)
+	);
 
 	return {
 		render: input,
 		getValue: getValue,
 		isValid: inputState,
 		shakeLabel: shakeLabel,
-	}
-}
+	};
+};
 
-export default useInput
+export default useInput;
