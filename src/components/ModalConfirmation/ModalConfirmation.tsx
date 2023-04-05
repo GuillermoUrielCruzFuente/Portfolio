baa62ec8cfac0f972f1de74dfe6b78518b137070
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-04-04 23:20:40 -0600

    feat: add a basic modal structure and an event to close it

--- src/components/ModalConfirmation/ModalConfirmation.tsx
+++ src/components/ModalConfirmation/ModalConfirmation.tsx
@@ -1,16 +1,38 @@
+import Button from "../Button/Button";
 import styles from "./ModalConfirmation.module.scss";
+import { Transition, motion } from "framer-motion";
+import okIcon from "@images/icons/ok.svg";
 
 export type ModalConfirmationProps = {
 	message: string;
 };
 
 const ModalConfirmation = ({ message }: ModalConfirmationProps) => {
+	const transitionConfig: Transition = { type: "keyframes", duration: 0.35 };
+
 	return (
-		<div className={styles.veil}>
-			<section>
+		<motion.div
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+			transition={transitionConfig}
+			className={styles.veil}
+		>
+			<div className={styles.modalContainer}>
 				<p>{message}</p>
-			</section>
-		</div>
+				
+				<Button
+					icon={okIcon}
+					onClick={() => {
+						const closeModalEvent = new CustomEvent("close-modal");
+
+						dispatchEvent(closeModalEvent);
+					}}
+				>
+					Entendido
+				</Button>
+			</div>
+		</motion.div>
 	);
 };
 
