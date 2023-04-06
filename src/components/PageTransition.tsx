ba1ea62d7edfc3830728977728ea0532b50f4a96
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-04-06 13:07:03 -0600

    style: update to the new formatting standard and add some ideas in the comments

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -1,5 +1,5 @@
-import { motion, Variants } from 'framer-motion'
-import { useEffect } from 'react'
+import { motion, Variants } from "framer-motion";
+import { useEffect } from "react";
 
 const variants: Variants = {
 	initial: {
@@ -11,18 +11,37 @@ const variants: Variants = {
 	exit: {
 		opacity: 0,
 	},
-}
+};
 
 const PageTransition = ({ children }: { children: JSX.Element }) => {
 	useEffect(() => {
-		window.scrollTo(0, 0)
-	}, [])
+		window.scrollTo(0, 0);
+	}, []);
 
+	/**
+	 * While using css-transition was posible to make animations without affect
+	 * the stacking context, although, the framer-motion migration has been broken
+	 * that stacking context. Now, is imposible to put the modal window in front of
+	 * navbar.
+	 *
+	 * It is possible that I can get rid of this by removing the setted transition
+	 * attributes (not really sure)
+	 *
+	 * Also, I can put the modal outside and increase the z-index in order to
+	 * achieve this behavior
+	 *
+	 * Or even better, I can make a cool transition in order to hide the navbar every
+	 * time a modal shows up
+	 *
+	 */
 	return (
-		<motion.div {...variants} transition={{ duration: 0.35 }}>
+		<motion.div
+			{...variants}
+			transition={{ duration: 0.35 }}
+		>
 			{children}
 		</motion.div>
-	)
-}
+	);
+};
 
-export default PageTransition
+export default PageTransition;
