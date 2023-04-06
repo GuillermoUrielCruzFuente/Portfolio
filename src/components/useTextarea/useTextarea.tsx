bd7356980c6fef8e4b1ed2e8aac1da627fed0579
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-04-06 12:51:32 -0600

    style: update to the new formatting standard

--- src/components/useTextarea/useTextarea.tsx
+++ src/components/useTextarea/useTextarea.tsx
@@ -1,66 +1,58 @@
-import { CSSProperties, useRef, useState } from 'react'
+import { CSSProperties, useRef, useState } from "react";
 
 type UseInput = {
-	render: JSX.Element
-	getValue: () => string
-	isValid: boolean
-	shakeLabel: () => void
-}
+	render: JSX.Element;
+	getValue: () => string;
+	isValid: boolean;
+	shakeLabel: () => void;
+};
 
 export type TextareaConfig = {
-	name: string
-	img: string
-	required: boolean
-}
+	name: string;
+	img: string;
+	required: boolean;
+};
 
-export const useTextarea = ({
-	name,
-	img,
-	required,
-}: TextareaConfig): UseInput => {
-	const textareaRef = useRef<HTMLTextAreaElement>(null)
-	const [textareaState, setTextareaState] = useState(false)
-	const labelRef = useRef<HTMLSpanElement>(null)
+export const useTextarea = ({ name, img, required }: TextareaConfig): UseInput => {
+	const textareaRef = useRef<HTMLTextAreaElement>(null);
+	const [textareaState, setTextareaState] = useState(false);
+	const labelRef = useRef<HTMLSpanElement>(null);
 
-	const getValue = () =>
-		textareaRef.current ? textareaRef.current.value : ''
+	const getValue = () => (textareaRef.current ? textareaRef.current.value : "");
 
 	const handleTextareaContentChange = () => {
-		if (textareaRef.current?.validity.valid && getValue() != '') {
-			setTextareaState(true)
+		if (textareaRef.current?.validity.valid && getValue() != "") {
+			setTextareaState(true);
 		} else {
-			setTextareaState(false)
+			setTextareaState(false);
 		}
-	}
+	};
 
 	const shakeLabel = () => {
-		labelRef.current!.style.animation = 'shake 400ms ease'
+		labelRef.current!.style.animation = "shake 400ms ease";
 
-		labelRef.current!.addEventListener(
-			'animationend',
-			(event: AnimationEvent) => {
-				if (event.animationName === 'shake') {
-					labelRef.current!.style.animation = 'unset'
-				}
+		labelRef.current!.addEventListener("animationend", (event: AnimationEvent) => {
+			if (event.animationName === "shake") {
+				labelRef.current!.style.animation = "unset";
 			}
-		)
-	}
+		});
+	};
 
 	const LABEL_STATE_COLORS = {
-		OK: '#99fb9c',
-		ERROR: '#fb9999',
-		NEUTRAL: '#f5c8ff',
-	}
+		OK: "#99fb9c",
+		ERROR: "#fb9999",
+		NEUTRAL: "#f5c8ff",
+	};
 
 	const computeLabelColor = (): CSSProperties => {
 		if (textareaState) {
-			return { color: LABEL_STATE_COLORS.OK }
+			return { color: LABEL_STATE_COLORS.OK };
 		} else if (required) {
-			return { color: LABEL_STATE_COLORS.ERROR }
+			return { color: LABEL_STATE_COLORS.ERROR };
 		} else {
-			return { color: LABEL_STATE_COLORS.NEUTRAL }
+			return { color: LABEL_STATE_COLORS.NEUTRAL };
 		}
-	}
+	};
 
 	const textarea = (
 		<>
@@ -77,7 +69,11 @@ export const useTextarea = ({
 
 				<label>{name}</label>
 
-				<img className="label-icon" src={img} alt={`${name} icon`} />
+				<img
+					className="label-icon"
+					src={img}
+					alt={`${name} icon`}
+				/>
 			</div>
 
 			<span
@@ -88,14 +84,14 @@ export const useTextarea = ({
 				Coloca aquí el mensaje que desees
 			</span>
 		</>
-	)
+	);
 
 	return {
 		render: textarea,
 		getValue: getValue,
 		isValid: textareaState,
 		shakeLabel: shakeLabel,
-	}
-}
+	};
+};
 
-export default useTextarea
+export default useTextarea;
