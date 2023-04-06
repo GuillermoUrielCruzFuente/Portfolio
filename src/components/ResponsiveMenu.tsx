f8b5608319bb8a896ba16cacd74772bcd9cf7bdc
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-04-06 13:07:43 -0600

    style: update to the new formatting standard and add some ideas in the comments

--- src/components/ResponsiveMenu.tsx
+++ src/components/ResponsiveMenu.tsx
@@ -1,43 +1,47 @@
-import { useState } from 'react'
-import useLottie from '@/hooks/useLottie'
-import hamMenu from '@lottie/hamburger-menu.json'
-import '@styles/components/ResponsiveMenu.scss'
-import changeScrollbarState from '@/helpers/ChangeScrollbarState'
-import ResponsiveMenuContainer from './ResponsiveMenuContainer'
+import { useState } from "react";
+import useLottie from "@/hooks/useLottie";
+import hamMenu from "@lottie/hamburger-menu.json";
+import "@styles/components/ResponsiveMenu.scss";
+import changeScrollbarState from "@/helpers/ChangeScrollbarState";
+import ResponsiveMenuContainer from "./ResponsiveMenuContainer";
 
 const ResponsiveMenu = () => {
-	const [HamMenu, HamMenuLottie] = useLottie({ data: hamMenu })
-	const [isOpen, setIsOpen] = useState(false)
+	const [HamMenu, HamMenuLottie] = useLottie({ data: hamMenu });
+	const [isOpen, setIsOpen] = useState(false);
 
-	const showBackArrow = () =>
-		HamMenuLottie.current.playSegments([20, 70], true)
+	const showBackArrow = () => HamMenuLottie.current.playSegments([20, 70], true);
 
-	const showHamburger = () =>
-		HamMenuLottie.current.playSegments([120, 140], true)
+	const showHamburger = () => HamMenuLottie.current.playSegments([120, 140], true);
 
 	const toggler = (currentState: boolean) => {
-		changeScrollbarState({ isVisible: currentState })
-		currentState ? showHamburger() : showBackArrow()
-		return !currentState
-	}
+		changeScrollbarState({ isVisible: currentState });
+		currentState ? showHamburger() : showBackArrow();
+		return !currentState;
+	};
 
-	const toggle = () => setIsOpen(toggler)
+	const toggle = () => setIsOpen(toggler);
 
 	const hideMenu = () => {
-		setIsOpen(false)
-		showHamburger()
-		changeScrollbarState({ isVisible: true })
-	}
+		setIsOpen(false);
+		showHamburger();
+		changeScrollbarState({ isVisible: true });
+	};
 
 	return (
 		<>
 			<div className="hamburger-menu-container">
-				<HamMenu className="hamburger-animation" onClick={toggle} />
+				<HamMenu
+					className="hamburger-animation"
+					onClick={toggle}
+				/>
 			</div>
 
-			<ResponsiveMenuContainer isOpen={isOpen} itemCallback={hideMenu} />
+			<ResponsiveMenuContainer
+				isOpen={isOpen}
+				itemCallback={hideMenu}
+			/>
 		</>
-	)
-}
+	);
+};
 
-export default ResponsiveMenu
+export default ResponsiveMenu;
