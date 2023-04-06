cc1d45451f4cb31e016841eecbd7d1a6145ca14e
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-04-06 13:08:52 -0600

    style: update to the new formatting standard and add some ideas in the comments

--- src/components/ResponsiveMenuContainer.tsx
+++ src/components/ResponsiveMenuContainer.tsx
@@ -1,19 +1,19 @@
-import { AnimatePresence, motion, Variants } from 'framer-motion'
-import '@styles/components/ResponsiveMenuContainer.scss'
-import { routes } from '@routes/index'
-import FancyLink from './FancyLink'
+import { AnimatePresence, motion, Variants } from "framer-motion";
+import "@styles/components/ResponsiveMenuContainer.scss";
+import { routes } from "@routes/index";
+import FancyLink from "./FancyLink";
 
 type ResponsiveMenuProps = {
-	isOpen: boolean
-	itemCallback: () => void
-}
+	isOpen: boolean;
+	itemCallback: () => void;
+};
 
 const mountingVariants: Variants = {
 	unmount: {
 		opacity: 0,
-		y: '-100%',
+		y: "-100%",
 		transition: {
-			type: 'spring',
+			type: "spring",
 			bounce: 1.5,
 			stiffness: 150,
 			damping: 10,
@@ -23,18 +23,15 @@ const mountingVariants: Variants = {
 		opacity: 1,
 		y: 0,
 		transition: {
-			type: 'spring',
+			type: "spring",
 			bounce: 1,
 			stiffness: 120,
 			damping: 8,
 		},
 	},
-}
+};
 
-const ResponsiveMenuContainer = ({
-	isOpen,
-	itemCallback,
-}: ResponsiveMenuProps) => {
+const ResponsiveMenuContainer = ({ isOpen, itemCallback }: ResponsiveMenuProps) => {
 	return (
 		<AnimatePresence>
 			{isOpen && (
@@ -45,7 +42,7 @@ const ResponsiveMenuContainer = ({
 					exit={{ opacity: 0 }}
 					transition={{
 						duration: 0.35,
-						type: 'tween',
+						type: "tween",
 					}}
 				>
 					<motion.ol
@@ -74,7 +71,7 @@ const ResponsiveMenuContainer = ({
 				</motion.div>
 			)}
 		</AnimatePresence>
-	)
-}
+	);
+};
 
-export default ResponsiveMenuContainer
+export default ResponsiveMenuContainer;
