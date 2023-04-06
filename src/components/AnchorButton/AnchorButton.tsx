a7936a35cfd92783c9561e945af6d65b839ddfef
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-04-06 12:43:35 -0600

    style: update to the new formatting standard

--- src/components/AnchorButton/AnchorButton.tsx
+++ src/components/AnchorButton/AnchorButton.tsx
@@ -1,19 +1,22 @@
 type AnchorButtonType = {
-	children: string
-	href: string
-	icon: string
-	primary?: true
-}
+	children: string;
+	href: string;
+	icon: string;
+	primary?: true;
+};
 
 const AnchorButton = ({ children, href, icon, primary }: AnchorButtonType) => (
 	<a
-		className={`project-button ${primary ? 'primary' : 'secondary'}`}
+		className={`project-button ${primary ? "primary" : "secondary"}`}
 		href={href}
 		target="_blank"
 	>
-		<img src={icon} alt="project info icon" />
+		<img
+			src={icon}
+			alt="project info icon"
+		/>
 		{children}
 	</a>
-)
+);
 
-export default AnchorButton
+export default AnchorButton;
