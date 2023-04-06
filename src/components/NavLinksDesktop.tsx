ab3b9769b782e30eb02e28411724fc0d18ab5537
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-04-06 12:56:58 -0600

    style: update to the new formatting standard

--- src/components/NavLinksDesktop.tsx
+++ src/components/NavLinksDesktop.tsx
@@ -1,41 +1,41 @@
-import { routes } from '@/routes'
-import '@styles/components/NavLinksDesktop.scss'
-import FancyLink from '@components/FancyLink'
-import { motion, Variants } from 'framer-motion'
-import { useLocation } from 'react-router-dom'
-import { useLogoAnimationState } from '@/context/LogoAnimationState/useLogoAnimationState'
+import { routes } from "@/routes";
+import "@styles/components/NavLinksDesktop.scss";
+import FancyLink from "@components/FancyLink";
+import { motion, Variants } from "framer-motion";
+import { useLocation } from "react-router-dom";
+import { useLogoAnimationState } from "@/context/LogoAnimationState/useLogoAnimationState";
 
 const mountingVariants: Variants = {
 	unmount: {
 		opacity: 0,
-		y: '-100%',
+		y: "-100%",
 	},
 	mount: {
 		opacity: 1,
 		y: 0,
 		transition: {
-			type: 'spring',
+			type: "spring",
 			bounce: 1,
 			stiffness: 120,
 			damping: 8,
 			duration: 1,
 		},
 	},
-}
+};
 
 const NavLinksDesktop = () => {
-	const { pathname } = useLocation()
-	const isHome = pathname === '/'
-	const [logoAnimationState] = useLogoAnimationState()
-	const hasLogoAnimationPlayed = logoAnimationState.isComplete
+	const { pathname } = useLocation();
+	const isHome = pathname === "/";
+	const [logoAnimationState] = useLogoAnimationState();
+	const hasLogoAnimationPlayed = logoAnimationState.isComplete;
 
 	//manage the delay value based on 2 conditions
 	//if it's home page or not
 	//and if the logo animation has been completed (home or nav)
 	const calculateDelay = (): number => {
-		const delay = isHome ? 2.8 : 1.5
-		return hasLogoAnimationPlayed ? 0 : delay
-	}
+		const delay = isHome ? 2.8 : 1.5;
+		return hasLogoAnimationPlayed ? 0 : delay;
+	};
 
 	return (
 		<motion.ol
@@ -48,12 +48,15 @@ const NavLinksDesktop = () => {
 			}}
 		>
 			{routes.map((route) => (
-				<motion.li key={route.path} variants={mountingVariants}>
+				<motion.li
+					key={route.path}
+					variants={mountingVariants}
+				>
 					<FancyLink to={route.path}>{route.text}</FancyLink>
 				</motion.li>
 			))}
 		</motion.ol>
-	)
-}
+	);
+};
 
-export default NavLinksDesktop
+export default NavLinksDesktop;
