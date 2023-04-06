22953782ce5b6dbcdad32c62a659d8384b876508
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-04-06 12:46:09 -0600

    style: update to the new formatting standard

--- src/components/IntersectionContainer/IntersectionContainer.tsx
+++ src/components/IntersectionContainer/IntersectionContainer.tsx
@@ -1,68 +1,65 @@
-import { CSSProperties, useLayoutEffect, useRef } from 'react'
+import { CSSProperties, PropsWithChildren, useLayoutEffect, useRef } from "react";
 
-type ToAppear = {
-	children: JSX.Element
-	transitionTime: number
-	from: CSSProperties
-	to: CSSProperties
-}
+export type IntersectionContainerAttributes = {
+	transitionTime: number;
+	from: CSSProperties;
+	to: CSSProperties;
+};
+
+type IntersectionContainerProps = PropsWithChildren<IntersectionContainerAttributes>;
 
 const IntersectionContainer = ({
 	children,
 	from,
 	to,
 	transitionTime,
-}: ToAppear) => {
-	const containerRef = useRef<HTMLDivElement>(null)
+}: IntersectionContainerProps) => {
+	const containerRef = useRef<HTMLDivElement>(null);
 
-	const computeEntries = (
-		entries: Array<IntersectionObserverEntry>,
-		observer: IntersectionObserver
-	) => {
+	const computeEntries: IntersectionObserverCallback = (entries, observer) => {
 		entries.forEach((entry) => {
 			if (entry.isIntersecting) {
 				for (let key in to) {
-					const value = to[key as keyof typeof to] as string //type assertion -> https://bobbyhadz.com/blog/typescript-no-index-signature-with-parameter-of-type-string
-					const property = key as any
-					containerRef.current!.style[property] = value
+					const value = to[key as keyof typeof to] as string; //type assertion -> https://bobbyhadz.com/blog/typescript-no-index-signature-with-parameter-of-type-string
+					const property = key as any;
+					containerRef.current!.style[property] = value;
 				}
 
-				observer.disconnect()
+				observer.disconnect();
 			}
-		})
-	}
+		});
+	};
 
 	const intersectionOptions: IntersectionObserverInit = {
 		root: null,
-		rootMargin: '0px',
+		rootMargin: "0px",
 		threshold: 0.5,
-	}
+	};
 
 	const setContainerTransition = () => {
 		if (containerRef.current) {
-			containerRef.current.style.transitionProperty =
-				Object.keys(to).join()
+			containerRef.current.style.transitionProperty = Object.keys(to).join();
 
-			containerRef.current.style.transitionDuration = `${transitionTime}ms`
+			containerRef.current.style.transitionDuration = `${transitionTime}ms`;
 		}
-	}
+	};
 
 	useLayoutEffect(() => {
-		setContainerTransition()
+		setContainerTransition();
 
-		const observer = new IntersectionObserver(
-			computeEntries,
-			intersectionOptions
-		)
+		const observer = new IntersectionObserver(computeEntries, intersectionOptions);
 
-		observer.observe(containerRef.current!)
-	}, [])
+		observer.observe(containerRef.current!);
+	}, []);
 
 	return (
-		<div style={from} ref={containerRef}>
+		<div
+			style={from}
+			ref={containerRef}
+		>
 			{children}
 		</div>
-	)
-}
+	);
+};
 
-export default IntersectionContainer
+export default IntersectionContainer;
