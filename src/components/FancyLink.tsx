5df450f042676fef5c3beb3cde171141f439156c
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-04-06 12:55:13 -0600

    style: update to the new formatting standard

--- src/components/FancyLink.tsx
+++ src/components/FancyLink.tsx
@@ -1,29 +1,32 @@
-import { NavLink, NavLinkProps } from 'react-router-dom'
-import '@styles/components/FancyLink.scss'
+import { NavLink, NavLinkProps } from "react-router-dom";
+import "@styles/components/FancyLink.scss";
 
-interface FancyLinkProps extends Omit<NavLinkProps, 'children' | 'className'> {
-	children: string
+interface FancyLinkProps extends Omit<NavLinkProps, "children" | "className"> {
+	children: string;
 }
 
-const classNameProperty = 'className'
-type LinkClassHandlerUnion = NavLinkProps[typeof classNameProperty]
-type LinkClassHandler = Extract<LinkClassHandlerUnion, Function>
+const classNameProperty = "className";
+type LinkClassHandlerUnion = NavLinkProps[typeof classNameProperty];
+type LinkClassHandler = Extract<LinkClassHandlerUnion, Function>;
 
 const FancyLink = (props: FancyLinkProps) => {
-	const { children, ...otherProps } = props
+	const { children, ...otherProps } = props;
 
 	const classHandler: LinkClassHandler = ({ isActive, isPending }) => {
-		return `fancy-link-base${isActive ? ' fancy-link-active' : ''}${
-			isPending ? ' fancy-link-pending' : ''
-		}`
-	}
+		return `fancy-link-base${isActive ? " fancy-link-active" : ""}${
+			isPending ? " fancy-link-pending" : ""
+		}`;
+	};
 
 	return (
-		<NavLink className={classHandler} {...otherProps}>
+		<NavLink
+			className={classHandler}
+			{...otherProps}
+		>
 			<span className="text">{children}</span>
 			<span className="text-hover">{children}</span>
 		</NavLink>
-	)
-}
+	);
+};
 
-export default FancyLink
+export default FancyLink;
