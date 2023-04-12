36ebe2adfd49d3bdb7aa12617471ddbe9b7140cf
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-04-11 22:20:09 -0600

    feat: add Veil component

---
+++ src/components/Veil/Veil.tsx
@@ -0,0 +1,21 @@
+import { PropsWithChildren } from "react";
+import styles from "./Veil.module.scss";
+import { Transition, motion } from "framer-motion";
+
+const Veil = ({ children }: PropsWithChildren) => {
+	const transitionConfig: Transition = { type: "keyframes", duration: 0.35 };
+
+	return (
+		<motion.div
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+			transition={transitionConfig}
+			className={styles["veil"]}
+		>
+			{children}
+		</motion.div>
+	);
+};
+
+export default Veil;
---
+++ src/components/Veil/index.ts
@@ -0,0 +1,1 @@
+export { default as Veil } from './Veil';
