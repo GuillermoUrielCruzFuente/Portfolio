e377092d9c94555d43c3d3d3d46a5fc2c11d188a
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-04-12 23:07:10 -0600

    feat: add variants to handle mount and unmount animations

--- src/components/LoadingNotification/LoadingNotification.tsx
+++ src/components/LoadingNotification/LoadingNotification.tsx
@@ -1,18 +1,44 @@
 import styles from "./LoadingNotification.module.scss";
 import Loader from "../Loader/Loader";
+import { motion, Variants } from "framer-motion";
+import { ANTICIPATE_TRANSITION } from "@/data/TransitionConfigurations";
 
 export type NotificationProps = {
 	message: string;
 	className?: string;
 };
 
+const loadingNotificationVariants: Variants = {
+	unmounted: {
+		opacity: 0,
+		scale: 0.35,
+		rotate: 20,
+		transition: ANTICIPATE_TRANSITION,
+	},
+	mounted: {
+		opacity: 1,
+		scale: 1,
+		rotate: 0,
+		transition: {
+			...ANTICIPATE_TRANSITION,
+			delay: 0.15,
+		},
+	},
+};
+
 const LoadingNotification = ({ message, className }: NotificationProps) => {
 	return (
-		<div className={`${styles.container}${className ? " " + className : ""}`}>
+		<motion.div
+			initial="unmounted"
+			animate="mounted"
+			exit="unmounted"
+			variants={loadingNotificationVariants}
+			className={`${styles.container}${className ? " " + className : ""}`}
+		>
 			<Loader className={styles.loader} />
 
 			<p className={styles.message}>{message}</p>
-		</div>
+		</motion.div>
 	);
 };
 
