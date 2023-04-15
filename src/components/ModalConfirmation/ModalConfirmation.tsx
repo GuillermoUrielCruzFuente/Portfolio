6e4e36bddc90150b5a751c9c0db2c62d481e7e22
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-04-14 23:16:11 -0600

    feat: update transitions and remove veil code

--- src/components/ModalConfirmation/ModalConfirmation.tsx
+++ src/components/ModalConfirmation/ModalConfirmation.tsx
@@ -1,40 +1,52 @@
 import Button from "../Button/Button";
 import styles from "./ModalConfirmation.module.scss";
-import { Transition, motion } from "framer-motion";
+import { Variants, motion } from "framer-motion";
 import okIcon from "@images/icons/ok.svg";
+import { ANTICIPATE_TRANSITION } from "@/data/TransitionConfigurations";
 
 export type ModalConfirmationProps = {
 	message: string;
 };
 
-const ModalConfirmation = ({ message }: ModalConfirmationProps) => {
-	const transitionConfig: Transition = { type: "keyframes", duration: 0.35 };
+const modalConfirmationVariants: Variants = {
+	unmounted: {
+		opacity: 0,
+		scale: 0.35,
+		rotate: 20,
+		transition: ANTICIPATE_TRANSITION,
+	},
+	mounted: {
+		opacity: 1,
+		scale: 1,
+		rotate: 0,
+		transition: ANTICIPATE_TRANSITION,
+	},
+};
 
+const ModalConfirmation = ({ message }: ModalConfirmationProps) => {
 	const emitCloseModalEvent = () => {
 		dispatchEvent(new CustomEvent("close-modal"));
 	};
 
 	return (
 		<motion.div
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			exit={{ opacity: 0 }}
-			transition={transitionConfig}
-			className={styles.veil}
+			initial="unmounted"
+			animate="mounted"
+			exit="unmounted"
+			variants={modalConfirmationVariants}
+			className={styles["modal-container"]}
 		>
-			<div className={styles["modal-container"]}>
-				<div className={styles["animation-container"]}></div>
+			<div className={styles["animation-container"]}></div>
 
-				<div className={styles["description-container"]}>
-					<p>{message}</p>
+			<div className={styles["description-container"]}>
+				<p>{message}</p>
 
-					<Button
-						icon={okIcon}
-						onClick={emitCloseModalEvent}
-					>
-						Entendido
-					</Button>
-				</div>
+				<Button
+					icon={okIcon}
+					onClick={emitCloseModalEvent}
+				>
+					Entendido
+				</Button>
 			</div>
 		</motion.div>
 	);
