9bd445ef6fb26815429a3ae6fa1175d18c686728
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-04-18 22:58:12 -0600

    chore: remove unused component

--- src/components/FormModal/FormModal.tsx
+++
@@ -1,7 +0,0 @@
-import { ModalConfirmation } from "../ModalConfirmation";
-
-const FormModal = () => {
-	return <ModalConfirmation message="hola"></ModalConfirmation>;
-};
-
-export default FormModal;
