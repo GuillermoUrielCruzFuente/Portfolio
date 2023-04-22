22595cabe02ab8b06448e014f20c810a28e16f2e
GuillermoUrielCruzFuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-04-21 22:32:36 -0600

    feat: add a global value for dev mode

---
+++ src/helpers/IsDevMode.ts
@@ -0,0 +1,7 @@
+/**
+ * Boolean value that indicates if the site is running in DevMode
+ *
+ * based on vite docs
+ * https://vitejs.dev/guide/env-and-mode.html#env-variables
+ */
+export const isDevMode = import.meta.env.DEV;
