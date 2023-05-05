36a0459009dedf712485b5f64c55ba7a0819788b
GuillermoUrielCruzFuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-05-04 19:06:43 -0600

    feat: include new types for FancyInput component

--- src/components/FancyInput/FancyInput.tsx
+++ src/components/FancyInput/FancyInput.tsx
@@ -15,76 +15,88 @@ export type FancyInputElement = HTMLInputElement & {
 	shakeInfoLabel: () => void;
 };
 
-export interface FancyInputProps
-	extends Omit<InputHTMLAttributes<HTMLInputElement>, "placeholder"> {
+/**
+ * An union type that include the input attributes
+ * that will be omitted in the final FancyInputAttributes type
+ */
+type InputExclusion = "placeholder";
+
+/**
+ *
+ */
+type InputInheritedAttributes = Omit<InputHTMLAttributes<HTMLInputElement>, InputExclusion>;
+
+export interface FancyInputAttributes extends InputInheritedAttributes {
 	labelText: string;
 	rotateIcon?: number;
 	iconSrc?: string;
 }
 
-const FancyInput = forwardRef((props: FancyInputProps, ref: ForwardedRef<FancyInputElement>) => {
-	const fancyInputRef = useRef<FancyInputElement>(null);
-	const labelInfoRef = useRef<HTMLParagraphElement>(null);
-	const [isValid, setIsValid] = useState(false);
+const FancyInput = forwardRef(
+	(props: FancyInputAttributes, ref: ForwardedRef<FancyInputElement>) => {
+		const fancyInputRef = useRef<FancyInputElement>(null);
+		const labelInfoRef = useRef<HTMLParagraphElement>(null);
+		const [isValid, setIsValid] = useState(false);
 
-	useEffect(() => {
-		document.documentElement.style.setProperty(
-			"--input-height",
-			`${fancyInputRef.current?.offsetHeight}px`
+		useEffect(() => {
+			document.documentElement.style.setProperty(
+				"--input-height",
+				`${fancyInputRef.current?.offsetHeight}px`
+			);
+		}, []);
+
+		const { labelText, rotateIcon, iconSrc, ...otherProps } = props;
+
+		useImperativeHandle(
+			ref,
+			() =>
+				({
+					shakeInfoLabel: () => {
+						fancyInputRef.current?.focus();
+
+						if (labelInfoRef.current) {
+							labelInfoRef.current.style.color = "crimson";
+							labelInfoRef.current.style.transition = "color 300ms";
+
+							setTimeout(() => {
+								labelInfoRef.current!.style.color = "white";
+							}, 300);
+						}
+					},
+					isValid,
+				} as FancyInputElement)
 		);
-	}, []);
 
-	const { labelText, rotateIcon, iconSrc, ...otherProps } = props;
+		const handleOnChange: ChangeEventHandler<HTMLInputElement> = (keyEvent) => {
+			if (fancyInputRef.current?.value === "") {
+				setIsValid(false);
+			} else {
+				setIsValid(true);
+			}
+		};
 
-	useImperativeHandle(
-		ref,
-		() =>
-			({
-				shakeInfoLabel: () => {
-					fancyInputRef.current?.focus();
+		return (
+			<>
+				<div className={styles["container"]}>
+					<input
+						ref={fancyInputRef}
+						onChange={handleOnChange}
+						placeholder={labelText}
+						{...otherProps}
+					/>
 
-					if (labelInfoRef.current) {
-						labelInfoRef.current.style.color = "crimson";
-						labelInfoRef.current.style.transition = "color 300ms";
+					<label>{labelText}</label>
 
-						setTimeout(() => {
-							labelInfoRef.current!.style.color = "white";
-						}, 300);
-					}
-				},
-				isValid,
-			} as FancyInputElement)
-	);
+					<img
+						src={iconSrc}
+						alt=""
+					/>
+				</div>
 
-	const handleOnChange: ChangeEventHandler<HTMLInputElement> = (keyEvent) => {
-		if (fancyInputRef.current?.value === "") {
-			setIsValid(false);
-		} else {
-			setIsValid(true);
-		}
-	};
-
-	return (
-		<>
-			<div className={styles["container"]}>
-				<input
-					ref={fancyInputRef}
-					onChange={handleOnChange}
-					placeholder={labelText}
-					{...otherProps}
-				/>
-
-				<label>{labelText}</label>
-
-				<img
-					src={iconSrc}
-					alt=""
-				/>
-			</div>
-
-			<p ref={labelInfoRef}>this is the default label</p>
-		</>
-	);
-});
+				<p ref={labelInfoRef}>this is the default label</p>
+			</>
+		);
+	}
+);
 
 export default FancyInput;
