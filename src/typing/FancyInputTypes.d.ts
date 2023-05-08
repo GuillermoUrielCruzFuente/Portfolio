dd9ff4069b07ad0fdbaecb2ee44a55e5f5bcb110
GuillermoUrielCruzFuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-05-07 19:26:17 -0600

    feat: split fancy input types into other file

---
+++ src/typing/FancyInputTypes.d.ts
@@ -0,0 +1,28 @@
+import type { InputHTMLAttributes } from "react";
+
+/**
+ *
+ */
+export type FancyInputElement = HTMLInputElement & {
+	shakeInfoLabel: () => void;
+};
+
+/**
+ * An union type that include the input attributes
+ * that will be omitted in the final FancyInputAttributes type
+ */
+export type InputExclusion = "placeholder";
+
+/**
+ *
+ */
+export type InputInheritedAttributes = Omit<InputHTMLAttributes<HTMLInputElement>, InputExclusion>;
+
+/**
+ *
+ */
+export interface FancyInputAttributes extends InputInheritedAttributes {
+	labelText: string;
+	feedbackText: string;
+	iconSrc: string;
+}
