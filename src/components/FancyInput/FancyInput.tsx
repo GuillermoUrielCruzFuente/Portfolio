423b237a31e8a0ecfcd0680a63cbcf21dccbff09
GuillermoUrielCruzFuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-05-21 21:02:12 -0600

    feat: improve validity reactivity and improve the nesting by passing FC to ForwardRef

--- src/components/FancyInput/FancyInput.tsx
+++ src/components/FancyInput/FancyInput.tsx
@@ -2,62 +2,66 @@ import { forwardRef, ForwardedRef, useImperativeHandle, useRef } from "react";
 import { FancyInputAttributes, FancyInputElement } from "@typing/FancyInputTypes";
 import styles from "./FancyInput.module.scss";
 
-/**
- *
- */
-const FancyInput = forwardRef(
-	(props: FancyInputAttributes, ref: ForwardedRef<FancyInputElement>) => {
-		const { labelText, feedbackText, iconSrc, ...otherProps } = props;
-		const fancyInputRef = useRef<FancyInputElement>(null);
-		const labelInfoRef = useRef<HTMLParagraphElement>(null);
+const FancyInput = (props: FancyInputAttributes, ref: ForwardedRef<FancyInputElement>) => {
+	const { labelText, feedbackText, iconSrc, ...otherProps } = props;
 
-		const shakeInfoLabel = () => {
-			const handleAnimationEnd = (event: AnimationEvent) => {
-				if (event.animationName === styles["shake"]) {
-					labelInfoRef.current!.classList.remove(styles["shake"]);
-					labelInfoRef.current!.removeEventListener("animationend", handleAnimationEnd);
-				}
-			};
+	const fancyInputRef = useRef<FancyInputElement>(null);
+	const iconRef = useRef<HTMLImageElement>(null);
+	const labelInfoRef = useRef<HTMLParagraphElement>(null);
 
-			if (labelInfoRef.current) {
-				labelInfoRef.current.classList.add(styles["shake"]);
-				labelInfoRef.current.addEventListener("animationend", handleAnimationEnd);
-			}
-		};
-
-		useImperativeHandle(ref, () => {
+	useImperativeHandle(
+		ref,
+		() => {
 			return {
 				shakeInfoLabel,
-				iconComputedSize: fancyInputRef.current!.offsetHeight - 24,
+				iconComputedSize: iconRef.current?.offsetHeight,
+				validity: fancyInputRef.current?.validity,
 			} as FancyInputElement;
-		});
+		},
+		[fancyInputRef.current?.validity, iconRef.current?.offsetHeight]
+	);
 
-		return (
-			<div>
-				<div className={styles["input-container"]}>
-					<input
-						ref={fancyInputRef}
-						placeholder={labelText}
-						{...otherProps}
-					/>
+	const handleFeedbackParagraphAnimationEnd = (event: AnimationEvent) => {
+		if (event.animationName === styles["shake"]) {
+			labelInfoRef.current!.classList.remove(styles["shake"]);
+			labelInfoRef.current!.removeEventListener(
+				"animationend",
+				handleFeedbackParagraphAnimationEnd
+			);
+		}
+	};
 
-					<label>{labelText}</label>
+	const shakeInfoLabel = () => {
+		labelInfoRef.current?.classList.add(styles["shake"]);
+		labelInfoRef.current?.addEventListener("animationend", handleFeedbackParagraphAnimationEnd);
+	};
 
-					<img
-						src={iconSrc}
-						alt=""
-					/>
-				</div>
+	return (
+		<div>
+			<div className={styles["input-container"]}>
+				<input
+					ref={fancyInputRef}
+					placeholder={labelText}
+					{...otherProps}
+				/>
 
-				<p
-					className={styles["feedback-paragraph"]}
-					ref={labelInfoRef}
-				>
-					{feedbackText}
-				</p>
+				<label>{labelText}</label>
+
+				<img
+					src={iconSrc}
+					alt={labelText + " input icon"}
+					ref={iconRef}
+				/>
 			</div>
-		);
-	}
-);
 
-export default FancyInput;
+			<p
+				className={styles["feedback-paragraph"]}
+				ref={labelInfoRef}
+			>
+				{feedbackText}
+			</p>
+		</div>
+	);
+};
+
+export default forwardRef(FancyInput);
