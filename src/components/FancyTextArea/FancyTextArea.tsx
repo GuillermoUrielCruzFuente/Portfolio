732f4ee2d77222149b0b3340af8fd6f215950bff
GuillermoUrielCruzFuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-05-22 21:49:49 -0600

    feat: add textarea validity property

--- src/components/FancyTextArea/FancyTextArea.tsx
+++ src/components/FancyTextArea/FancyTextArea.tsx
@@ -5,59 +5,70 @@ import styles from "./FancyTextArea.module.scss";
 /**
  *
  */
-const FancyTextArea = forwardRef(
-	(props: FancyTextAreaAttributes, ref: ForwardedRef<FancyTextAreaElement>) => {
-		const { labelText, feedbackText, iconSrc, iconSize, ...otherProps } = props;
-		const fancyTextAreaRef = useRef<FancyTextAreaElement>(null);
-		const labelInfoRef = useRef<HTMLParagraphElement>(null);
+const FancyTextArea = (props: FancyTextAreaAttributes, ref: ForwardedRef<FancyTextAreaElement>) => {
+	const { labelText, feedbackText, iconSrc, iconSize, ...otherProps } = props;
 
-		const shakeInfoLabel = () => {
-			const handleAnimationEnd = (event: AnimationEvent) => {
-				if (event.animationName === styles["shake"]) {
-					labelInfoRef.current!.classList.remove(styles["shake"]);
-					labelInfoRef.current!.removeEventListener("animationend", handleAnimationEnd);
-				}
-			};
+	const fancyTextAreaRef = useRef<FancyTextAreaElement>(null);
+	const labelInfoRef = useRef<HTMLParagraphElement>(null);
 
-			if (labelInfoRef.current) {
-				labelInfoRef.current.classList.add(styles["shake"]);
+	const handleFeedbackParagraphAnimationEnd = (event: AnimationEvent) => {
+		if (event.animationName === styles["shake"]) {
+			labelInfoRef.current!.classList.remove(styles["shake"]);
+			labelInfoRef.current!.removeEventListener(
+				"animationend",
+				handleFeedbackParagraphAnimationEnd
+			);
+		}
+	};
 
-				labelInfoRef.current.addEventListener("animationend", handleAnimationEnd);
-			}
-		};
+	const shakeInfoLabel = () => {
+		if (labelInfoRef.current) {
+			labelInfoRef.current.classList.add(styles["shake"]);
+			labelInfoRef.current.addEventListener(
+				"animationend",
+				handleFeedbackParagraphAnimationEnd
+			);
+		}
+	};
 
-		useImperativeHandle(ref, () => {
-			return { shakeInfoLabel } as FancyTextAreaElement;
-		});
+	useImperativeHandle(
+		ref,
+		() => {
+			return {
+				shakeInfoLabel,
+				validity: fancyTextAreaRef.current?.validity,
+			} as FancyTextAreaElement;
+		},
+		[fancyTextAreaRef.current?.validity]
+	);
 
-		return (
-			<div>
-				<div className={styles["textarea-container"]}>
-					<textarea
-						ref={fancyTextAreaRef}
-						placeholder={labelText}
-						rows={5}
-						{...otherProps}
-					/>
+	return (
+		<div>
+			<div className={styles["textarea-container"]}>
+				<textarea
+					ref={fancyTextAreaRef}
+					placeholder={labelText}
+					rows={5}
+					{...otherProps}
+				/>
 
-					<label>{labelText}</label>
+				<label>{labelText}</label>
 
-					<img
-						src={iconSrc}
-						alt=""
-						width={iconSize}
-					/>
-				</div>
-
-				<p
-					className={styles["feedback-paragraph"]}
-					ref={labelInfoRef}
-				>
-					{feedbackText}
-				</p>
+				<img
+					src={iconSrc}
+					alt=""
+					width={iconSize}
+				/>
 			</div>
-		);
-	}
-);
 
-export default FancyTextArea;
+			<p
+				className={styles["feedback-paragraph"]}
+				ref={labelInfoRef}
+			>
+				{feedbackText}
+			</p>
+		</div>
+	);
+};
+
+export default forwardRef(FancyTextArea);
