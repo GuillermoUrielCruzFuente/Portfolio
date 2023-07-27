df1cc089c86578817e9c26006b5562459c5b0fc9
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-07-26 22:31:23 -0600

    chore: specify type import

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -1,4 +1,4 @@
-import { HTMLAttributes } from "react";
+import type { HTMLAttributes } from "react";
 import styles from "./PageContainer.module.scss";
 
 type PageContainerAttributes = HTMLAttributes<HTMLDivElement> & {};
