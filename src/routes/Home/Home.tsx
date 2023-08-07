fae4c7f460b9d06aa2f4b6fe7004addd9d51990a
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-08-06 18:55:09 -0600

    feat: home new animatin wip

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import SocialMedia from "@components/SocialMedia/SocialMedia";
 import Button from "@components/Button/Button";
 import useLottie from "@/hooks/useLottie";
@@ -9,34 +9,101 @@ import contactIcon from "@images/icons/home-buttons/plane.svg";
 import collage from "@images/web-images/collage.png";
 import { PageContainer } from "@components/PageContainer";
 import "./Home.scss";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Home = () => {
 	const collageRef = useRef<HTMLImageElement>(null);
 	const [_, setLogoAnimationState] = useLogoAnimationState();
-	const [LogoAnimation, LogoLottie] = useLottie({
+	const [LogoAnimationComponent, logoLottieRef] = useLottie({
 		data: logoAnimationData,
 	});
 
 	useEffect(() => {
-		LogoLottie.current.play();
+		setTimeout(() => {
+			logoLottieRef.current.play();
+		}, 500);
 
-		LogoLottie.current.addEventListener("complete", dispatchCompleteLogoState);
+		logoLottieRef.current.addEventListener("complete", onLogoAnimationComplete);
 	}, []);
 
-	const dispatchCompleteLogoState = () => setLogoAnimationState({ isComplete: true });
+	const logoAnimationContainerRef = useRef<HTMLDivElement>(null);
+	const onLogoAnimationComplete = () => {
+		setLogoAnimationState({ isComplete: true });
+
+		setTimeout(() => {
+			setHomeState(true);
+		}, 500);
+	};
 
 	const showCollage = () => {
 		collageRef.current!.classList.replace("collage-init", "collage-final");
 	};
 
+	const [homeState, setHomeState] = useState(false);
+
 	return (
-		<PageContainer addNavbarMarginTop>
-			<div className="home-content">
-				<p className="big-text">Hola! 👋🏾 soy</p>
+		<PageContainer
+			addNavbarMarginTop
+			fillVerticalViewport
+			className="home-page-container"
+		>
+			<AnimatePresence mode="wait">
+				{!homeState ? (
+					<motion.div
+						ref={logoAnimationContainerRef}
+						className="logo-animation-container"
+						initial={{ opacity: 0 }}
+						animate={{
+							opacity: 1,
+							transition: {
+								duration: 1,
+								ease: "linear",
+							},
+						}}
+						exit={{ opacity: 0 }}
+						key="logo-container"
+					>
+						<LogoAnimationComponent className="lottie-animation" />
+					</motion.div>
+				) : (
+					<motion.p
+						key="home-content"
+						initial={{ opacity: 0 }}
+						animate={{
+							opacity: 1,
+							transition: {
+								duration: 0.4,
+							},
+						}}
+						exit={{ opacity: 0 }}
+						className="big-text"
+					>
+						Hola! 👋🏾
+					</motion.p>
+				)}
+			</AnimatePresence>
 
-				<LogoAnimation className="lottie-animation" />
-
-				<p className="description">
+			{/* <motion.div
+				layout
+				className="layout-composer"
+			>
+				{isAnimationComplete.isComplete && (
+					<motion.p
+						initial={{ opacity: 0, y: "20%" }}
+						animate={{
+							opacity: 1,
+							y: 0,
+							transition: {
+								duration: 0.4,
+							},
+						}}
+						className="big-text"
+					>
+						Hola! 👋🏾 soy
+					</motion.p>
+				)}
+			</motion.div> */}
+			{/* <p className="description">
 					<span className="accent">Desarrollador Frontend</span> de tiempo completo, con
 					más de 3 años de experiencia. Mexicano, con intervención en distintos proyectos
 					profesionales, una gran creatividad y atención a los detalles.
@@ -75,7 +142,7 @@ const Home = () => {
 						onLoad={showCollage}
 					/>
 				</div>
-			</div>
+			</div> */}
 		</PageContainer>
 	);
 };
