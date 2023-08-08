f00535455a6efebb8fed2c4b937654104b40ddb9
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-08-07 22:39:36 -0600

    feat: add fill-vertical-viewport attribute

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -1,21 +1,41 @@
-import type { HTMLAttributes } from "react";
+import { useRef, type HTMLAttributes, useEffect } from "react";
 import styles from "./PageContainer.module.scss";
 
 type PageContainerAttributes = HTMLAttributes<HTMLDivElement> & {
 	addNavbarMarginTop?: boolean;
+	fillVerticalViewport?: boolean;
 };
 
 const PageContainer = ({
 	children,
 	className,
 	addNavbarMarginTop,
+	fillVerticalViewport,
 	...otherAttributes
 }: PageContainerAttributes) => {
+	const containerRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const containerElement = containerRef.current;
+
+		addNavbarMarginTop
+			? containerElement?.setAttribute("add-navbar-margin-top", "")
+			: containerElement?.removeAttribute("add-navbar-margin-top");
+
+		fillVerticalViewport
+			? containerElement?.setAttribute("fill-vertical-viewport", "")
+			: containerElement?.removeAttribute("fill-vertical-viewport");
+	}, [addNavbarMarginTop, fillVerticalViewport]);
+
 	const getClassNames = () => {
 		const classes: Array<string> = [];
 
 		className && classes.push(className);
+
 		addNavbarMarginTop && classes.push(styles["navbar-margin-top"]);
+
+		fillVerticalViewport && classes.push(styles["fill-vertical-viewport"]);
+
 		classes.push(styles["page-container"]);
 
 		return classes.join(" ");
@@ -24,6 +44,7 @@ const PageContainer = ({
 	return (
 		<div
 			className={getClassNames()}
+			ref={containerRef}
 			{...otherAttributes}
 		>
 			{children}
