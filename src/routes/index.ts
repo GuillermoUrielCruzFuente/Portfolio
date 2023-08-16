be2d29bf3c0e25850e503ad6e72d14978823e263
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-08-15 20:43:14 -0600

    style: apply new prettier config

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,35 +1,35 @@
-export type Pages = 'Home' | 'About' | 'Projects' | 'Blog' | 'Contact'
+export type Pages = "Home" | "About" | "Projects" | "Blog" | "Contact";
 
 export type PagesDictionary<T> = {
-	[Page in Pages]: T
-}
+	[Page in Pages]: T;
+};
 
 export type AppRoute = {
-	text: string
-	path: string
-}
+	text: string;
+	path: string;
+};
 
 export const AppRoutes: PagesDictionary<AppRoute> = {
 	Home: {
-		path: '/',
-		text: 'Inicio',
+		path: "/",
+		text: "Inicio",
 	},
 	About: {
-		path: '/sobre-mi',
-		text: 'Sobre mí',
+		path: "/sobre-mi",
+		text: "Sobre mí",
 	},
 	Projects: {
-		path: '/proyectos',
-		text: 'Proyectos',
+		path: "/proyectos",
+		text: "Proyectos",
 	},
 	Blog: {
-		path: '/blog',
-		text: 'Blog',
+		path: "/blog",
+		text: "Blog",
 	},
 	Contact: {
-		path: '/contacto',
-		text: 'Contacto 👋',
+		path: "/contacto",
+		text: "Contacto 👋",
 	},
-}
+};
 
-export const routes = Object.values(AppRoutes)
+export const routes = Object.values(AppRoutes);
