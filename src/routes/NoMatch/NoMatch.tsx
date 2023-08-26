495e8f42fe55490527f8cd7f3ac841794b68106b
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-08-25 19:12:45 -0600

    feat: add module styles

--- src/routes/NoMatch/NoMatch.tsx
+++ src/routes/NoMatch/NoMatch.tsx
@@ -2,7 +2,7 @@ import { useLayoutEffect } from "react";
 import monsterAnimationData from "@lottie/404-pet.json";
 import useLottie from "@/hooks/useLottie";
 import { PageContainer } from "@components/PageContainer";
-import "./NoMatch.scss";
+import styles from "./NoMatch.module.scss";
 
 export default function NoMatch() {
 	const [Monster, MonsterLottie] = useLottie({
@@ -19,13 +19,13 @@ export default function NoMatch() {
 		<PageContainer
 			fillVerticalViewport
 			addNavbarMarginTop
-			id="no-match"
+			className={styles["no-match"]}
 		>
-			<div className="monster-container">
-				<Monster className="monster-animation" />
+			<div className={styles["monster-container"]}>
+				<Monster className={styles["monster-animation"]} />
 			</div>
 
-			<h1 className="error-text">Error 404</h1>
+			<h1 className={styles["error-title"]}>Error 404</h1>
 
 			<p>La página a la que intentas acceder nunca existió, o quizás sí...</p>
 		</PageContainer>
