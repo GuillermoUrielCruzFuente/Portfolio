7be74dde61b6b441f389a7ee49edffe4fbef7e05
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-09-06 21:21:47 -0600

    chore: update NavLinksDesktop component import

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,51 +1,50 @@
-import { useEffect, useRef, useState } from 'react'
-import NavLogo from '@components/NavLogo'
-import NavLinksDesktop from '@components/NavLinksDesktop'
-import ResponsiveMenu from '../ResponsiveMenu'
-import './Nav.scss'
+import { useEffect, useRef, useState } from "react";
+import NavLogo from "@components/NavLogo";
+import { NavLinksDesktop } from "@components/NavLinksDesktop";
+import ResponsiveMenu from "../ResponsiveMenu";
+import "./Nav.scss";
 
 const Nav = () => {
-	const navRef = useRef<HTMLElement>(null)
-	const [isLargeScreen, setIsLargeScreen] = useState(false)
+	const navRef = useRef<HTMLElement>(null);
+	const [isLargeScreen, setIsLargeScreen] = useState(false);
 
 	const changeStyleOnScroll = () => {
 		if (window.scrollY >= 100) {
-			navRef.current?.classList.replace('no-blur-bg', 'blur-bg')
+			navRef.current?.classList.replace("no-blur-bg", "blur-bg");
 		} else {
-			navRef.current?.classList.replace('blur-bg', 'no-blur-bg')
+			navRef.current?.classList.replace("blur-bg", "no-blur-bg");
 		}
-	}
+	};
 
 	const screenResizeHandler = () => {
-		window.innerWidth > 860
-			? setIsLargeScreen(true)
-			: setIsLargeScreen(false)
-	}
+		window.innerWidth > 860 ? setIsLargeScreen(true) : setIsLargeScreen(false);
+	};
 
 	useEffect(() => {
-		screenResizeHandler()
-		changeStyleOnScroll()
+		screenResizeHandler();
+		changeStyleOnScroll();
 
-		window.addEventListener('scroll', changeStyleOnScroll)
-		window.addEventListener('resize', screenResizeHandler)
+		window.addEventListener("scroll", changeStyleOnScroll);
+		window.addEventListener("resize", screenResizeHandler);
 
 		return () => {
-			window.removeEventListener('scroll', changeStyleOnScroll)
-			window.removeEventListener('resize', screenResizeHandler)
-		}
-	}, [])
+			window.removeEventListener("scroll", changeStyleOnScroll);
+			window.removeEventListener("resize", screenResizeHandler);
+		};
+	}, []);
 
 	return (
-		<nav ref={navRef} className="no-blur-bg">
+		<nav
+			ref={navRef}
+			className="no-blur-bg"
+		>
 			<div id="nav-container">
 				<NavLogo />
 
-				<div id="navigator">
-					{isLargeScreen ? <NavLinksDesktop /> : <ResponsiveMenu />}
-				</div>
+				<div id="navigator">{isLargeScreen ? <NavLinksDesktop /> : <ResponsiveMenu />}</div>
 			</div>
 		</nav>
-	)
-}
+	);
+};
 
-export default Nav
+export default Nav;
