ce00fd9868c038cec4f1f7a643040624cce4cac9
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-09-08 23:58:51 -0600

    chore: remove comment

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -18,22 +18,6 @@ const PageTransition = ({ children }: { children: JSX.Element }) => {
 		window.scrollTo(0, 0);
 	}, []);
 
-	/**
-	 * While using css-transition was posible to make animations without affect
-	 * the stacking context, although, the framer-motion migration has been broken
-	 * that stacking context. Now, is imposible to put the modal window in front of
-	 * navbar.
-	 *
-	 * It is possible that I can get rid of this by removing the setted transition
-	 * attributes (not really sure)
-	 *
-	 * Also, I can put the modal outside and increase the z-index in order to
-	 * achieve this behavior
-	 *
-	 * Or even better, I can make a cool transition in order to hide the navbar every
-	 * time a modal shows up
-	 *
-	 */
 	return (
 		<motion.div
 			{...variants}
