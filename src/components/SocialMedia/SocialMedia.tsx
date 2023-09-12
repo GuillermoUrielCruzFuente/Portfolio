3dc198d3b17fcce39541faa33acc0e44ea81a08e
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-09-11 21:25:15 -0600

    chore: add SocialMedia folder component

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -1,14 +1,14 @@
 import socialMediaSites from "@/data/SocialMediaData";
 import { SocialMediaLink } from "@components/SocialMediaLink";
-import "./SocialMedia.scss";
+import styles from "./SocialMedia.module.scss";
 
 type SocialMediaProps = {
 	containerClass: string;
 	state?: boolean;
 };
 
-const SocialMedia = ({ containerClass }: SocialMediaProps) => (
-	<div className={`social-media-main-container ${containerClass}`}>
+export const SocialMedia = ({ containerClass }: SocialMediaProps) => (
+	<div className={`${styles["social-media-main-container"]} ${containerClass}`}>
 		{socialMediaSites.map((site) => (
 			<SocialMediaLink
 				key={site.name}
@@ -17,5 +17,3 @@ const SocialMedia = ({ containerClass }: SocialMediaProps) => (
 		))}
 	</div>
 );
-
-export default SocialMedia;
