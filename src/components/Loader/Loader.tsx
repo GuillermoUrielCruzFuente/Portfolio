24d957364d01ced6848b0c484d8b05a19eddb3bf
Guillermo Cruz <guillermo.uriel.cruz.fuente@gmail.com>
2023-09-29 16:34:01 -0600

    feat: add size customization [WIP]

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,9 +1,10 @@
+import { CSSProperties } from "react";
 import styles from "./Loader.module.scss";
 
 type LoaderProps = {
 	className?: string;
 	shadow?: true;
-	size?: "small" | "medium" | "large";
+	size?: "small" | "medium" | "large" | number;
 };
 
 export const Loader = ({ shadow, size = "small", className }: LoaderProps) => {
@@ -29,6 +30,17 @@ export const Loader = ({ shadow, size = "small", className }: LoaderProps) => {
 	};
 
 	const containerClasses = () => {
+		// todo: change css values based on user size definition
+		// css properties to change
+		// - tile-grid width
+		// - central-shadow width
+		if (typeof size === "number") {
+			console.log("specified with via prop: ", size);
+			const widthRule: CSSProperties = { width: size };
+
+			return;
+		}
+
 		const base = [styles[size], styles["tile-grid"]];
 		const classes = className ? [...base, className] : [...base];
 		return classes.join(" ");
